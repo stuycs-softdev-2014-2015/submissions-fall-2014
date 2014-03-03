0380f3e21ed33ce110d00b3ef3b0a2a1a0060c77
takchiwan <ray325789@yahoo.com>
2014-03-02 19:46:50 -0500

    Update app.py

--- 6/decorator/takchi_wan/app.py
+++ 6/decorator/takchi_wan/app.py
@@ -40,7 +40,7 @@ def testpage():
 @app.route('/logout')
 def logout():
 	session.pop("username")
-	return redirect(url_for('/login'))
+	return redirect(url_for('login'))
 	
 if __name__ == "__main__":
 	app.run(debug = True)
