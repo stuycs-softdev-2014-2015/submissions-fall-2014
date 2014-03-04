dbd5d5ee337c1aa92c54c83feeba07c35b340138
Christine Xu <christinexu1001@gmail.com>
2014-03-04 00:12:28 -0500

    oops

--- 7/decorator/christine_xu/app.py
+++ 7/decorator/christine_xu/app.py
@@ -6,20 +6,29 @@ from pymongo import MongoClient
 app = Flask(__name__)
 app.secret_key = "awesome"
 
+def auth(func, *args, **kwargs):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if "user" in session:
+            return func()
+        else:
+            return redirect(url_for("login"))
+    return wrapper
 
-@app.route('/',methods=["POST","GET"])
+@app.route('/', methods = ['GET', 'POST'])
 def login():
-    if request.method == "GET":
-        return render_template("Login.html")
-    if request.method == "POST":
-        return redirect(url_for("/Home"));
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        session['user'] = username
+    return redirect(url_for("index"))
 		
 @app.route('/Home', methods = ["POST", "GET"])
 def Home():
     if request.method == "GET":
         return render_template('Home.html')
-
-
 
 
 if __name__=="__main__":
