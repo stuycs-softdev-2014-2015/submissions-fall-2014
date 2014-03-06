49c34f52c5af8009023d01901eab28f7e08f366f
Oswoll <nicholas.galasinao@gmail.com>
2014-03-05 23:42:12 -0500

    done

--- 6/comprehensions/nicholas_galasinao/cipher.py
+++ 6/comprehensions/nicholas_galasinao/cipher.py
@@ -26,24 +26,14 @@ def encode2(s,n):
 
 import math
 def dist(a,b):
-    sum=0
+    dist=0
     #for i in range(len(a)):
     #    sum = sum + pow(a[i]-b[i],2)
-    sum=[ pow(a[x]-b[x],2) for x in len(a) ]
-    return math.sqrt(sum)
+    dist=[pow(a[x]-b[x],2) for x in len(a)]
+    return math.sqrt(dist)
 
 def calcPercents(s):
-    letters=[chr(x) for x in range(97,123)]
-    sLen = len(s)
-    count = 0
-    counts = [1 for l in s if l == letter]
-    for x in letters:
-        letter = letters[x]
-        for y in s:
-            if y == letter:
-                count++
-        percent = count / slen
-    percents=[ 
+    return [float(s.count(chr(x)))/len(s.replace(" ",""))*100 for x in range(97, 120)]
     """
     Returns a list of 26 elements L[0] will be the frequency of 
     the letter a i the list, L[1] the letter b etc. 
@@ -51,7 +41,7 @@ def calcPercents(s):
     You can calculate the frequence by calculating 
     (# times the letter appears)/(total # of letters)
     """
-    pass
+   
 
 englishPercents=[8.167,1.492,2.782,4.253,12.702,2.228,2.015,6.094,
                  6.966,0.153,0.772,4.025,2.406,6.749,7.507,1.929,0.095,
@@ -62,6 +52,15 @@ import random
 s="this is a sample sentence for use in testing the ceasar cipher thing"
 # This is encoded message
 encmessage = encode2(s,random.randrange(26))
+
+def calcJaneEyre():
+    t = open("1260.txt", "r")
+    return calcPercents(t.read())
+
+def decode(s):
+    percents = calcJaneEyre()
+    distance = [dist(calcPercents(encode2(s,x)), percents) for x in range(0,26)]
+    return encode2(s,distance.index(min(distance)))
 
 # Your tasks
 #1. Rewrite dist so that it uses a list comprehention
