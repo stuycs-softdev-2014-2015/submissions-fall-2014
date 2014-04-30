8080833536f7d51fdfdec684b74b0bf7432c81f1
Christopher Burke <cburke496@gmail.com>
2014-04-30 13:14:16 -0400

    Functions for switching data

--- 7/d3/chris/arrests.js
+++ 7/d3/chris/arrests.js
@@ -6,6 +6,38 @@ var svg = d3.select("body").append("svg")
     .attr("height",400)
     .attr("id","svg");
 
+var firstData = true;
+
+var dataSwitchMaker = function(s) {
+    var f = function() {
+	if (!firstData) {
+	    svg.selectAll("circle")
+		.data(data)
+		.transition()
+		.duration(1000)
+		.attr("cx",function(d) {return d.UrbanPop * 4;})
+		.attr("cy",function(d) {return -1 * d[s] + 400;})
+		.attr("fill","#ff0000")
+		.attr("r",5);
+        } else {
+	    svg.selectAll("circle")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr("cx",function(d) {return d.UrbanPop * 4;})
+		.attr("cy",function(d) {return -1 * d[s] + 400;})
+		.attr("fill","#ff0000")
+		.attr("r",5);
+	    firstData = false;
+	}
+    }
+    return f;
+}
+
+var assault = dataSwitchMaker("Assault");
+var murder = dataSwitchMaker("Murder");
+var rape = dataSwitchMaker("Rape");
+
 d3.csv("USArrests.csv")
     .row(function(d) {
 	return {State : d.State, Murder: d.Murder, Assault: d.Assault,
@@ -14,12 +46,5 @@ d3.csv("USArrests.csv")
     .get(function(error, rows) {
 	data = rows;
 	console.log(data);
-	svg.selectAll("circle")
-	    .data(data)
-	    .enter()
-	    .append("circle")
-	    .attr("cx",function(d) {return d.UrbanPop * 4;})
-	    .attr("cy",function(d) {return -1 * d.Assault + 400;})
-	    .attr("fill","#ff0000")
-	    .attr("r",5);
+	assault();
 	});
