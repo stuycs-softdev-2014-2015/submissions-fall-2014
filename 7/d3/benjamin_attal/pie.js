09b8b478b6b784098765214126a85c0f54064b3e
Benjamin Attal <attal.benjamin@gmail.com>
2014-05-04 01:13:01 -0400

    Working verison

--- 7/d3/benjamin_attal/pie.js
+++ 7/d3/benjamin_attal/pie.js
@@ -1,5 +1,5 @@
-var width = 900,
-    height = 900,
+var width = 700,
+    height = 700,
     radius = height / 2;
 
 var color = d3.scale.ordinal()
@@ -26,21 +26,59 @@ var svg = d3.select("body").append("svg")
 d3.csv("carts.csv", function(error, data) {
 
     //Bind the pie chart data to the parent of the paths
-    var g = svg.selectAll(".arc")
-        .data(pie(data))
+    var chart = svg.selectAll(".arc")
+        .data(pie(data), function(d) {return d.data.city;})
         .enter().append("g")
             .attr("class", "arc");
 
     //Paths will inherit pie chart data from the parent, which automatically fills in the size of the arc based on the data
-    g.append("path")
+    chart.append("path")
         .attr("d", arc)
         .style("fill", function(d) { return color(d.data.city); });
 
     //Append the text
-    g.append("text")
+    chart.append("text")
         .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
         .attr("dy", ".35em")
         .style("text-anchor", "middle")
         .text(function(d) { return d.data.city; });
 
+    chart.on('click', function(d, i) {
+        var a;
+        for(a = 0; a < data.length; a++) {
+            if (a !== i)
+                data[a].number = "0";
+        }
+
+        console.log(data);
+
+        var path = chart.selectAll("path");
+        chart.selectAll("text")
+            .text("");
+
+        path.each(function(d) { this._lastDatum = d; })
+            .data(pie(data), function(d) {return d.data.city;})
+            .transition()
+                .duration(500)
+                .attrTween("d", arcTween);
+
+        var message = "";
+        path.each(function(d) {
+            if (d.data.number != 0)
+                message = d.data.city + ", " + d.data.number;
+        });
+
+        d3.select("svg").append("text")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+            .attr("dy", ".35em")
+            .style("text-anchor", "middle")
+            .text(function(d) { return message; });
+    });
 });
+
+//Help from Edric Huang
+function arcTween(d) {
+    var i = d3.interpolate(this._lastDatum, d);
+    this._lastDatum = i(0);
+    return function(t) { return arc(i(t)); };
+}
