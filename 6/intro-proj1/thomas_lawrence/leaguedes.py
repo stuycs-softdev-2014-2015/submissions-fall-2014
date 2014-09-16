b1b2e117fe449029e77fda89aa19ae7d104535bf
OokamiNya <42chrono@gmail.com>
2014-09-16 18:38:03 -0400

    moshimoshi, flask table desu

--- 6/intro-proj1/thomas_lawrence/leaguedes.py
+++ 6/intro-proj1/thomas_lawrence/leaguedes.py
@@ -5,11 +5,20 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Welcome to the league of desu</h1>"
+    r = "<h1>Welcome to the league of desu</h1>"
+    r += '<a href="/desu">desu</a>'
+    return r
 
-@app.route("/itsudemo")
-def itsudemo():
-    return render_template("itsudemo.html")
+@app.route("/desu")
+def desu():
+    csvtable = []
+    csvf = open("data/out.csv")
+    csvln = csvf.readlines()
+    csvf.close()
+    for ln in csvln:
+        csvtable.append(ln.split(","))
+    return render_template("desu.html",
+                           csvtable=csvtable)
 
 if __name__ == "__main__":
     app.debug = True
