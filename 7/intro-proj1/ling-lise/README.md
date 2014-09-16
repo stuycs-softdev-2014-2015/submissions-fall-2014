779afb4fe6948df57284799703c8b5af435241e2
koprty <your_email@example.com>
2014-09-16 18:59:15 -0400

    flask web server debugged; spreadsheet appears via localhost.../conversion

--- 7/intro-proj1/ling-lise/README.md
+++ 7/intro-proj1/ling-lise/README.md
@@ -1,3 +1,21 @@
 This folder includes the mini-project of Ling Dong and Lise Ho.
 
-Details
+
+Instructions:
+Copy and paste py and csv files and place in virtualenv
+Copy html files in templates folder and place in corresponding templates folder in a virtualenv
+
+Python conversion.py
+
+Default Port is 5050
+localhost:5050/
+
+
+Log:
+conversion.py shoots to port 5050 with run with flask
+Currently has not been debugged.... freezes browser for some reason
+
+
+Update:
+conversion.py has been debugged
+
--- 7/intro-proj1/ling-lise/conversion.py
+++ 7/intro-proj1/ling-lise/conversion.py
@@ -3,29 +3,36 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 #global variable - holds string content
-htmlStr ="<table> <table border = '1'> <tr> <td> "
+htmlStr ="<table border = '1'> <tr> <td> \r"
 
 @app.route("/")
+def home ():
+    return render_template("test.html")
+
 @app.route("/conversion")
 def conversion (): #converts the csv file into html code
     ans = ""
-    filecsv = open ("data_hw25.csv", "r") # the data set with teh debate statistics 
+    filecsv = open ("data.csv", "r") # the data set with teh debate statistics 
     y = filecsv.readlines () # every line put into the a list of strings
     filecsv.close ()
     i =0
     lengths = len(y)
     while i < lengths :
         if i != lengths - 1: #if the index is not the last index of the list
-            ans += y[i].replace ("\n", "</td> </tr> <tr> <td>") # strings can use function replace to replace substrings
-            #/n symbolizes next line/ row for table : that means we need to end cell and row and add new row and cell if the element is not the last one
-        else: # last index of the list, add all the end tags to the html
-            ans += y[i].replace ("\n", " </td> </tr> </table> </div>  </body> </html> ") # we need to end all tags in last element
-            i += 1
-            ans = ans.replace (",", "</td> <td>") # for all the commas we need to end the cell for the nums and add one for the squares
+            ans += y[i].replace ("\n", "</td </tr> <tr> <td>\r") 
+        else: 
+            ans += y[i].replace ("\n", "</td> </tr> \r</table> </div>  </body> </html> ")
+            ans = ans.replace (",", "</td> <td>") 
             ans = ans.replace (";", " ")  # for some reason, google docs/libre office; one of them added ";" instead of a blank white space...
                                       #so to fix the problem, we added this line to replace the semi-colon with an actual space
-    return render_template("some.html",
-                           ans)
+        i += 1
+        
+
+    
+
+    return render_template("table.html",
+                           htmlStr=htmlStr,
+                           ans=ans)
 
 if __name__=="__main__":
     app.debug=True
