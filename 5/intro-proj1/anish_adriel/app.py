69428e8e9952adbbe9a612724f9f275b254cffec
BigHatMan <anishmalhotra97@gmail.com>
2014-09-16 11:39:15 -0400

    Updated app.py

--- 5/intro-proj1/anish_adriel/app.py
+++ 5/intro-proj1/anish_adriel/app.py
@@ -5,45 +5,20 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route("/help")
-def help():
-    return render_template("help.html")
-
 @app.route("/about")
 def about():
-    #s = "<h1>this is the about page</h1>"
-    s = """
-    <h1>Awesome Flask Web Site</h1>
-    <ul>
-    <li>item 1</li>
-    <li>item 2</li>
-    <li>item 3</li>
-    </ul>
-    """
-    return s
+    return render_template("about.html")
 
-@app.route("/random")
-def randomnumber():
-    import random
-    num = random.randrange(0,100)
-    return render_template("random.html",
-    num=num,
-    name="Mr. T",
-    l=[1,2,3,4,5],
-    d={'a':1,'two':2,3:'hello'})
-
-@app.route("/person/")
-@app.route("/person/<lastname>")
-@app.route("/person/<lastname>/<firstname>")
-def person(lastname=None,firstname=None):
-    return render_template("person.html",lastname=lastname,firstname=firstname)
+@app.route("/result/")
+@app.route("/result/<Type>")
+def result(Type=None):
+    return render_template("result.html",Type=Type)
 
 @app.route("/home")
 @app.route("/")
 def home():
-    return "<h1>This is the home page</h1>"
+    return render_template("home.html")
 
 if __name__=="__main__":
     app.debug=True
     app.run(host="0.0.0.0",port=6734)
-#app.run(host="0.0.0.0",port=8888)
