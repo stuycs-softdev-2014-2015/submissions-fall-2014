24fdc1e002d7a283ceef65e59e7527b7bdefba97
Mark Norwich <mna12@aol.com>
2014-09-16 20:23:40 -0400

    update

--- 6/intro-proj1/justin-mark/main.py
+++ 6/intro-proj1/justin-mark/main.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+
 @app.route("/data")
 def data():
-    l = open_file(f)
-    l.pop(0)
-    return render_template("data.html", d={"data":l})
+    y =  l
+    return render_template("data.html", d={"data":y})
     
 
 f='School_Attendance_and_Enrollment_Statistics_by_District__2010-11_.csv'
@@ -22,7 +22,47 @@ def open_file(filename):
     for line in open(filename).readlines():
         L.append (line.strip().split(","))
     return L
+l = open_file(f)
+l.pop(0)
 
+def findMax (l):
+    new = []
+    for i in l:
+        new.append(int(i[2]))
+    new.pop(len(new) -1)
+    return max(new)
+
+def findMin(l):
+    new = []
+    for i in l:
+        new.append(int(i[2]))
+    new.pop(len(new) -1)
+    new.pop(len(new) -1)
+    return min(new)
+def findMaxPercent(l):
+    new = []
+    for i in l:
+        new.append(i[1])
+    new.pop(len(new) -1)
+    new.pop(len(new) -1)
+    return max(new)
+
+def findMinPercent(l):
+    new = []
+    for i in l:
+        new.append(i[1])
+    new.pop(len(new) -1)
+    new.pop(len(new) -1)
+    return min(new)
+
+@app.route("/facts")    
+def facts():
+    x = findMax(l) 
+    y = findMin(l)
+    z = findMaxPercent(l)
+    a = findMinPercent(l)
+    return render_template("facts.html",x=x, y=y,z=z, a=a)
+        
 
 if __name__ == "__main__":
     app.debug = True
