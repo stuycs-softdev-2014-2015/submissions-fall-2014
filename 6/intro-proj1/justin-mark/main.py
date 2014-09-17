856424bcb13cfe4139f5bf4b14ef2c99628e8434
Justin Kim <yulli@yulli.org>
2014-09-16 20:41:13 -0400

    ¯\_(ツ)_/¯

--- 6/intro-proj1/justin-mark/main.py
+++ 6/intro-proj1/justin-mark/main.py
@@ -11,63 +11,61 @@ def home():
 
 @app.route("/data")
 def data():
-    y =  l
-    return render_template("data.html", d={"data":y})
-    
+    data = read_attendance_data()
+    data.pop(0)
 
-f='School_Attendance_and_Enrollment_Statistics_by_District__2010-11_.csv'
+    return render_template("data.html", d={"data": data})
 
 
-def open_file(filename):
-    L=[]
-    for line in open(filename).readlines():
-        L.append (line.strip().split(","))
-    return L
-l = open_file(f)
-l.pop(0)
-
-def findMax (l):
+def findMax(l):
     new = []
     for i in l:
         new.append(int(i[2]))
-    new.pop(len(new) -1)
+    new.pop(len(new) - 1)
     return max(new)
+
 
 def findMin(l):
     new = []
     for i in l:
         new.append(int(i[2]))
-    new.pop(len(new) -1)
-    new.pop(len(new) -1)
+    new.pop(len(new) - 1)
+    new.pop(len(new) - 1)
     return min(new)
+
+
 def findMaxPercent(l):
     new = []
     for i in l:
         new.append(i[1])
-    new.pop(len(new) -1)
-    new.pop(len(new) -1)
+    new.pop(len(new) - 1)
+    new.pop(len(new) - 1)
     return max(new)
+
 
 def findMinPercent(l):
     new = []
     for i in l:
         new.append(i[1])
-    new.pop(len(new) -1)
-    new.pop(len(new) -1)
+    new.pop(len(new) - 1)
+    new.pop(len(new) - 1)
     return min(new)
     data = read_attendance_data()
     data.pop(0)
     return render_template("data.html", d={"data": data})
 
 
-@app.route("/facts")    
+@app.route("/facts")
 def facts():
-    x = findMax(l) 
-    y = findMin(l)
-    z = findMaxPercent(l)
-    a = findMinPercent(l)
+    data = read_attendance_data()
+    data.pop(0)
+
+    x = findMax(data)
+    y = findMin(data)
+    z = findMaxPercent(data)
+    a = findMinPercent(data)
     return render_template("facts.html",x=x, y=y,z=z, a=a)
-        
+
 
 @app.route("/data/<order>")
 def data_order(order="attendance-desc"):
