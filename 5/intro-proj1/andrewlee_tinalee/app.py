752d7b5b7525c85be1dc01dd0fff95d3665ec6a5
Andrew Lee <andrew120.lee@gmail.com>
2014-09-17 00:40:33 -0400

    edited app

--- 5/intro-proj1/andrewlee_tinalee/app.py
+++ 5/intro-proj1/andrewlee_tinalee/app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/home")
 @app.route("/")
