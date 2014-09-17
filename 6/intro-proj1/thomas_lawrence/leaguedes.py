27c32cab4616ac2d4f80ee52ca96e54e1fb196af
thlebowicz <thomashlebowicz1@gmail.com>
2014-09-16 23:58:36 -0400

    Added Background and Second Table

--- 6/intro-proj1/thomas_lawrence/leaguedes.py
+++ 6/intro-proj1/thomas_lawrence/leaguedes.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    r = "<h1>Welcome to the league of desu</h1>"
+    r = '''<body background="static/leaguebackground.jpg">'''
+    r += "<h1>Welcome to the league of desu</h1>"
     r += '<a href="/desu">desu</a>'
+    r += '<br><a href="/kun">kun</a></body>'
     return r
 
 @app.route("/desu")
@@ -19,6 +21,17 @@ def desu():
         csvtable.append(ln.split(","))
     return render_template("desu.html",
                            csvtable=csvtable)
+
+@app.route("/kun")
+def kun():
+    csvtable = []
+    csvf = open("data/out.csv")
+    csvln = csvf.readlines()
+    csvf.close()
+    for ln in csvln:
+        csvtable.append(ln.split(","))
+        return render_template("kun.html",csvtable=csvtable,columnstoget = [0,1,2,4,6,8,10,12,14,16,18,19])
+
 
 if __name__ == "__main__":
     app.debug = True
