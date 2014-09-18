dbaebe2c8307f326bd2c84b443b0553c89ea8b0a
Merkuri <bz2118111@gmail.com>
2014-09-17 23:43:27 -0400

    restarted search and compare

--- 7/intro-proj1/tianshi-barak/app.py
+++ 7/intro-proj1/tianshi-barak/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 data_file = open("data.csv", "r")
 
@@ -16,41 +16,16 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    s = """
-    <h1>Welcome to our webpage.</h1>
-    <h2> Tianshi Wang, Barak Zhou, pd7 </h2>
-    <a href="../data">Check out our data!</a><br>
+    button = request.args.get("b",None)
+    search1 = request.args.get("search1",None)
+    search2 = request.args.get("search2",None)
+    print button, search1, search2
+    if button == None or button == "Search":
+        return render_template("survey.html");
 
-    <form action="../search/<search>" method="get">
-    Or search for a champion: <input type="text" name="search"><br>
-    <input type="submit" value="Search">
-    </form>    
-    """
-    return s
-
-@app.route("/search/<search>")
-def search(search, methods=['POST']):
-    s = """
-<h1 align="center"><b>Search Results</b></h1>
-"""
-    for x in data_text:
-        if x[0] == search:
-            s += """
-        <table border="2" cellspacing="1" cellpadding="5" align="center">
-        """
-            s+= "<tr>"
-            for y in range(0,len(x)):
-                if i<5:
-                    s += """<th bgcolor="#FFFF00"><b>"""+x[y].upper()+"</b></td>"
-                elif i%5==0: #for first row
-                    s += """<td bgcolor="#00FFCC"><b>""" + x[y] + "</b></td>"
-                else:
-                    s += """<td bgcolor="#FF66FF">""" + x[y] + "</td>"
-        s+= "</tr>"
-        s += "</table>"
-    return s
-
-
+@app.route("/results")
+def results():
+    ##returns the results of comparison
 
 @app.route("/data")
 def data():
