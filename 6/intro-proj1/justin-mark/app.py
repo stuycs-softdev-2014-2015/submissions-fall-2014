9413030e02ec4acd86ec59626bd2e03a576d8fa0
Justin Kim <yulli@yulli.org>
2014-09-19 13:50:57 -0400

    ¯\_(ツ)_/¯

--- 6/intro-proj1/justin-mark/app.py
+++ 6/intro-proj1/justin-mark/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from data import read_attendance_data, sort_data, find_facts
 
 app = Flask(__name__)
@@ -9,9 +9,23 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/data")
+@app.route("/data", methods=["GET", "POST"])
 def data():
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        data = read_attendance_data()
+        print request.form
+
+    return render_template("data.html", d={"data": data})
+
+
+@app.route("/data/<order>")
+def data_order(order="attendance-desc"):
     data = read_attendance_data()
+
+    if order:
+        data = sort_data(data, order)
 
     return render_template("data.html", d={"data": data})
 
@@ -28,16 +42,6 @@ def facts():
     a = facts['min_percent']
 
     return render_template("facts.html", x=x, y=y, z=z, a=a)
-
-
-@app.route("/data/<order>")
-def data_order(order="attendance-desc"):
-    data = read_attendance_data()
-
-    if order:
-        data = sort_data(data, order)
-
-    return render_template("data.html", d={"data": data})
 
 
 if __name__ == "__main__":
