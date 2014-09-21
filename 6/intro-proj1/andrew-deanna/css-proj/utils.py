211963dc25a5a1aa18cc7ba90e7bcbafd7d6e66b
Andrew Fischer <afischer15@mac.com>
2014-09-21 16:53:17 -0400

    Pictures of me, pictures of you...

---
+++ 6/intro-proj1/andrew-deanna/css-proj/utils.py
@@ -0,0 +1,16 @@
+import random
+
+def findPrice(size,issues):
+
+
+def findImg(size):
+  if size == "full":
+    return "Full"
+  if size == "half":
+    return "Half"
+  if size == "quarter":
+    return "Quarter"
+  if size == "eighth":
+    return "Eighth"
+  else:
+    return "AdSizes"
