0b316c858f73e6817bef65b586f99db1245582a1
ericwongg <wonggeric@gmail.com>
2014-09-21 16:37:49 -0400

    Main and comparision by draft works

--- 5/intro-proj1/junhao_ericwong/app.py
+++ 5/intro-proj1/junhao_ericwong/app.py
@@ -1,10 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
-
-@app.route("/")
-def main():
-    return render_template("main.html")
 
 one = open('./static/data/2012draft.csv', 'r')
 info1 = one.readlines()
@@ -37,9 +33,26 @@ def getanalysisinfo():
         analysis[pick] = [draft[index][0], draft[index][2], draftexpress[index][2], NBAdraft[index][2]]
     return analysis
 
-@app.route("/draft")
-def draft():
-    return render_template("draft.html",info=getanalysisinfo())
+@app.route("/",methods=["GET","POST"])
+def main():
+    b = request.args.get("b",None)
+    t = request.args.get("type",None)
+    chose1 = request.args.get("statistic",None)
+    chose2 = request.args.get("draft",None)
+    print b,t,chose1,chose2
+    l = ['Points','Rebounds','Assists','Field Goal','Free Throw']
+    if request.method=="GET":
+        return render_template("main.html",l=l)
+    else:
+        chose1 = request.form.getlist("statistic")
+        chose2 = request.form.getlist("d")
+        t = request.form['type']
+        b = request.form['b']
+        if b=="blah":
+            if t=="stat":
+                app.stat()
+            else:
+                return render_template("draft.html",info=getanalysisinfo(),l=chose2)
 
 @app.route("/stat")
 def stat():
