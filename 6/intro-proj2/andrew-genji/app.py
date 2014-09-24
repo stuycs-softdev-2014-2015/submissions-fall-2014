2d6a199f532a324e30c45fcbe54073d01ab8dc41
afischer15 <afischer15@mac.com>
2014-09-24 12:01:07 -0400

    FILESgs

---
+++ 6/intro-proj2/andrew-genji/app.py
@@ -0,0 +1,10 @@
+from flask import flask, render_template,
+import unittest
+import random
+
+###############################
+######## Intro-Proj 2 #########
+### A. Fischer, G. Noguchi  ###
+###############################
+
+def validate_password(pword)
