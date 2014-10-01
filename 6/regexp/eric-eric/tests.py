2bb4febfd7d5e221f7c7972c5cd918e140e9b5af
ezchen <echen2015@gmail.com>
2014-10-01 12:31:20 -0400

    finished writing first test

--- 6/regexp/eric-eric/tests.py
+++ 6/regexp/eric-eric/tests.py
@@ -1,5 +1,11 @@
 import unittest
+import filterName
+
 
 class TestRegexFunctions(unittest.TestCase):
     def setUp(self):
-        self.test
+        self.testString = "Hello this is a test String made by Eric Chen"
+
+    def testNameSearch(self):
+        names = filterName.getNames(self.testString)
+        assertIn("Eric Chen", names)
