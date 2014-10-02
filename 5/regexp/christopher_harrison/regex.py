f57d9b98a8a2eba947e9e43ecf5724ff90500703
Applesauce136 <harrison.chiu.136@gmail.com>
2014-10-02 11:28:51 -0400

    the last one wasn't good

--- 5/regexp/christopher_harrison/regex.py
+++ 5/regexp/christopher_harrison/regex.py
@@ -1,13 +1,22 @@
 import re
 
-def extract_names(string):
+def extract_names_regex(string):
     result = []
-    re_caps = re.compile('[A-Z][a-z]+')
+    re_caps = re.compile('^[A-Z][a-z]+.$')
     for word in string.split():
         if re_caps.match(word):
             result.append(word)
             print word
     return result
-        
-f = open("test/secret_service/secret_service.txt")
+
+def extract_names_list(string):
+    '''Matches each word against a list of names, and includes the word if it is found.'''
+    pass
+
+def extract_names_words(string):
+    '''Matches each word against a list of words, and includes the word if it is NOT found.'''
+    pass
+
+
+f = open("test_files/secret_service.txt")
 extract_names(f.read())
