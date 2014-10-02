67eb325261a08e4f099dec6c90462f67bc5d0e73
Matthew <mcookhome@gmail.com>
2014-10-02 12:08:38 -0400

    added misters

--- 6/regexp/brian_matt/findnames.py
+++ 6/regexp/brian_matt/findnames.py
@@ -5,7 +5,7 @@ g = f.read()
 f.close()
 
 def findnames():
-    p = re.compile('[A-Z][a-z]+(?:\s[A-Z][a-z]+)+')
+    p = re.compile('[A-Z][a-z]+\.*(?:\s[A-Z][a-z]+)+')
     L=p.findall(g)
     for i in xrange(len(L)):
         L[i] = L[i].replace('\n',' ')
