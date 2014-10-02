724ed08c6be479d37d034e327efdd0b4297888ee
apiccato <aida.piccato@gmail.com>
2014-10-02 13:20:40 -0400

    Adding misses

--- 7/regexp/aida_albert/regex.py
+++ 7/regexp/aida_albert/regex.py
@@ -4,8 +4,8 @@ f = open("sherlockholmes.txt")
 text = f.read()
 f.close()
 
-names = re.findall('[A-Z][a-z]+ [A-Z][a-z]+',text,flags=0)
+misses = re.match('Miss [A-Z][a-z]+ [A-Z][a-z]+|Miss [A-Z][a-z]+',text,flags=0)
 
 if __name__=="__main__":
-    print len(names)
-    print names[156]
+    
+    print misses.groups()
