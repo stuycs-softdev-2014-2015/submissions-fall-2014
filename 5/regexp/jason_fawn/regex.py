0a9843f418c4a6bd2c597640e9dd816586a18c79
jasonluX13 <jasonluX13@gmail.com>
2014-10-02 19:18:04 -0400

    Added firstnames

--- 5/regexp/jason_fawn/regex.py
+++ 5/regexp/jason_fawn/regex.py
@@ -1,8 +1,33 @@
 import re
 
-data = open("test.txt", "r")
+data = open("15414.txt", "r")
 text = data.read()
 data.close()
 
-matchNames = re.findall(r'([A-Z][a-z]+ [A-Z][a-z]+)', text)
-print matchNames
+fname = open("firstnames.txt", "r")
+firstnames = []
+for line in fname:
+    firstnames.append(line.split()[0].capitalize())
+fname.close()
+
+lname = open("lastnames.txt", "r")
+last = lname.read()
+lname.close()
+
+#firstnames = re.findall(r'[A-Z][a-z]', first)
+
+names = []
+def findNames(s):
+    matchNames = re.findall(r'([A-Z][a-z.-]+ [A-Z][a-z.-]+)', s)
+    ifName(matchNames)
+    matchNames = re.findall(r'([A-Z][a-z.-]+ [A-Z][a-z.-]+ [A-Z][a-z.-]+)', s)
+    ifName(matchNames)     
+    print names
+
+def ifName(l):
+    for name in l:
+        if name.split()[0] in firstnames:
+            names.append(name)
+
+            
+findNames(text)
