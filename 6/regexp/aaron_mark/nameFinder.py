843d8b54c06af1121d2d75dabd7492054083a4cd
Mark Norwich <mna12@aol.com>
2014-10-02 18:36:22 -0400

    couldnt resolve anything

--- 6/regexp/aaron_mark/nameFinder.py
+++ 6/regexp/aaron_mark/nameFinder.py
@@ -2,7 +2,8 @@
 import re
 
 reg1 = "(([DMS][ris]{1-3}[\.]? )?(Mr\. )?([A-Z][a-z]*)([\s-][A-Z][a-z]*))"
-FL = "([A-Z][a-z]*)([\s-][A-Z][a-z]*)"
-reg = "(((Mr\. )|(Mrs\. )|(Dr\. )|(Ms\. )|(Sir\. ){0,1})(([A-Z][a-z]*)([\s-][A-Z][a-z]*)))"
-print re.findall(reg,"Mark Norwich hey Dr.  there Mr. Cooper Weaver hi how are Mr. you Sam-Mortenson")
 
+
+reg = "((Mr\. )|(Mrs\. )|(Dr\. )|(Ms\. )|(Sir\. ){0,1})([A-Z][a-z]*[\s-][A-Z][a-z]*)"
+
+ret = re.findall(reg,"Mark Norwich hey Dr.  there Mr. Cooper Weaver hi how are Mr. you Dr. Sam-Mortenson")
