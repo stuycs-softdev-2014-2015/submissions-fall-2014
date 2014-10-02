b614bbbfe3ea4555ef4254ef6719397c7c18cdd4
ezchen <echen2015@gmail.com>
2014-10-01 23:16:35 -0400

    added more tests

--- 6/regexp/eric-eric/tests.py
+++ 6/regexp/eric-eric/tests.py
@@ -5,12 +5,39 @@ import filterName
 class TestRegexFunctions(unittest.TestCase):
     def setUp(self):
         self.testString = "Hello this is a test String made by Eric Chen"
+        self.testString2 = "Brian O'Malley"
+        self.testStringMultipleNames = "Brian O'Malley said hi to Eric Chen"
 
-    def testNameSearch(self):
+    def testNameSearchSimple(self):
         names = filterName.getNames(self.testString)
         self.assertIn("Eric Chen", names)
 
-if __name__=='__main__':
+    def testNameSearchApostrophe(self):
+        names = filterName.getNames(self.testString2)
+        self.assertIn("Brian O'Malley", names)
+
+
+class TestCleaningFunctions(unittest.TestCase):
+    def setUp(self):
+        self.exampleNames = ["Eric Chen", "Brian O'Malley", "Another Name"]
+        self.testString = """Eric Chen Eric Chen Eric Chen Eric Chen
+        Brian O'Malley Brian O'Malley
+        Another Name Another Name Another Name
+        """
+
+    def testRemoveDuplicates(self):
+        names = filterName.getNames(self.testString)
+        D = filterName.deleteDuplicates(names)
+        namecount = True
+
+        for name in self.exampleNames:
+            if D[name] != 3:
+                namecount = false
+
+        self.assertEqual(namecount, True)
+
+if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestRegexFunctions)
     unittest.TextTestRunner(verbosity=2).run(suite)
-
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCleaningFunctions)
+    unittest.TextTestRunner(verbosity=2).run(suite)
