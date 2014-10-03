27299a584bf759f37d195e19951e28b07cdb7176
Aaron Mortenson <aamortenson1@gmail.com>
2014-10-02 20:57:57 -0400

    updated title finder

--- 6/regexp/aaron_mark/nameFinder.py
+++ 6/regexp/aaron_mark/nameFinder.py
@@ -1,9 +1,10 @@
 
 import re
 
-reg1 = "(([DMS][ris]{1-3}[\.]? )?(Mr\. )?([A-Z][a-z]*)([\s-][A-Z][a-z]*))"
+reg1 = "(([DMS][ris]{1,3}\.? )?([A-Z][a-z]*)([\s-][A-Z][a-z]*))"
 
 
 reg = "((Mr\. )|(Mrs\. )|(Dr\. )|(Ms\. )|(Sir\. ){0,1})([A-Z][a-z]*[\s-][A-Z][a-z]*)"
 
-ret = re.findall(reg,"Mark Norwich hey Dr.  there Mr. Cooper Weaver hi how are Mr. you Dr. Sam-Mortenson")
+ret = re.findall(reg1,"Mark Norwich hey Dr.  there Mr. Cooper Weaver hi how are Mr. you Dr. Sam Mortenson")
+print ret
