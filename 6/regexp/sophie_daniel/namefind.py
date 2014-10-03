ebbde55478a1c819f79a850eb03db423210a7844
Zabari <zabari.daniel@Gmail.com>
2014-10-03 15:54:56 -0400

    dictionary

--- 6/regexp/sophie_daniel/namefind.py
+++ 6/regexp/sophie_daniel/namefind.py
@@ -1,12 +1,17 @@
 import re
 f=open("first.txt",'r')
 s=f.read()
+f.close()
 #p=re.compile("(([A-Z][a-z]*)\s([A-Z][a-z]*))")
 #p=re.compile("([A-Z][a-z]*\s*){2,}")
-p=re.compile("(([A-Z][a-z]*)\s([A-Z][a-z]*))")
-#print p.findall(s)
-r=re.compile("((?<!\.\s)(?<!\.\s\s)[A-Z][a-z]+)")
-#print r.findall(s)
-for name in r.findall(s):
-    print name+":" +str(s.count(name))
-
+p=re.compile("([A-Z][a-z]*)\s([A-Z][a-z]*)")
+pre= p.findall(s)  
+r=re.compile("((?<!\.\s)(?<!\.\s\s)(?<!\.\s\s\")(?<!\.\s\")(?<!\"\s)(?<!\"\s\s)(?<!\")[A-Z][a-z]+)")
+l=r.findall(s)
+for f in pre:
+    for se in f:
+        l.append(se)
+d={name: s.count(name) for name in l}
+#for name in r.findall(s):
+#    print name+":" +str(s.count(name))
+print d
