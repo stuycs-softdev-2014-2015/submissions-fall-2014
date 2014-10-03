284e510fe90d23de04112db360460150af881010
Victor Gaitour <Grimlon1234@gmail.com>
2014-10-02 21:09:23 -0400

    works with only single word names

--- 7/regexp/lise_victor/namere.py
+++ 7/regexp/lise_victor/namere.py
@@ -1,7 +1,12 @@
 import re
 
+textfile = open("names.csv", "r")
+names= textfile.read()
+textfile.close()
+names= names.split()
 
-fname="JackWinters.txt"
+fname="testfile.txt"
+
 def opentestfile(fname):
     f = open (fname, "r")
     x= f.read () 
@@ -10,21 +15,36 @@ def opentestfile(fname):
 
 
 
-x = "Cats are smarter than dogs. This is the opinion of Alex."
-x = "abcdef"
-x = "Mr. Stone found little Amy in Mr. Eric Smith 65364562345243523"
-#x = opentestfile(fname)
+
+#x = "Cats are smarter than dogs. This is the opinion of Alex."
+#x = "abcdef"
+#x = "Mr. Stone found little Amy in Mr. Eric Smith 65364562345243523"
+x = opentestfile(fname)
 def findname():
     '''
     m= re.search(r".[^ab]+",x) #bcdef
     m= re.search(r"[a-f]+",x) #abcdef
     '''
-    m = re.findall("[A-Z][a-z\./].+",x)
+    m = re.findall("[A-Z][a-z]+",x)
+    #m = re.findall("[A-Z][a-z\./].+",x)
     #m = re.findall("[^Mr.][A-Z]?[\w.]+ ",x)
+    #print m
+    #namesfound= []
+    #for name in m:
+    #    namesfound.append(name)
     
-    print m
-   
+    #m = re.findall("[^Mr.][A-Z]?[\w.]+ ",x)
+    matches=[y for y in m if y in names]
+    numbnames={}
+    for name in matches:
+        if name in numbnames:
+            numbnames[name]+=1
+        else:
+            numbnames[name]=1
+    return numbnames
+    #return matches
+    
 
 
 
-findname()
+print findname()
