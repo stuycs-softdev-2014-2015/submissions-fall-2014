a6333d2ba61ae6939f6f637e0bd61fcdecf9b388
StevenZabolotny <stevezabolotny@gmail.com>
2014-10-03 13:17:26 -0400

    Added list of exempt strings, doesn't work for some reason.

--- 7/regexp/Steven-Tianshi/regexp.py
+++ 7/regexp/Steven-Tianshi/regexp.py
@@ -4,6 +4,18 @@ f = open("metamorphosis.txt")
 s = f.read()
 f.close()
 
-stuff = re.findall("(Mr\.|Ms\.|Mrs\.) ([A-Z][a-z]+ [A-Z][a-z]+|[A-Z][a-z]+)",s)
-stuff.append(re.findall("[^\.\?!] ([A-Z][a-z]+ [A-Z][a-z]+|[A-Z][a-z]+)",s))
+def check(s):
+    for e in exempt:
+        if (s == e):
+            print(s + e)
+            return True
+    return False
+
+stuff = re.findall("(Mr\.|Ms\.|Mrs\.|Mr|Ms|Mrs) ([A-Z][a-z]+ [A-Z][a-z]+|[A-Z][a-z]+)",s)
+stuff2 = re.findall("[^\.\?!] ([A-Z][a-z]+ [A-Z][a-z]+|[A-Z][a-z]+)",s)
+exempt = ["Mr.","Ms.","Mrs.","Mr","Mrs","Ms"]
+[stuff2.remove(x) for x in stuff2 if (check(x))]
+stuff.extend(stuff2)
 print stuff
+
+
