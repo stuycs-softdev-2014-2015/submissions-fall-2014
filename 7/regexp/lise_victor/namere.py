a17c534589ee90479cddcc416a8e4d81d550f26d
koprty <your_email@example.com>
2014-10-03 17:20:02 -0400

    updated regex file

--- 7/regexp/lise_victor/namere.py
+++ 7/regexp/lise_victor/namere.py
@@ -9,39 +9,41 @@ fname="JackWinters.txt"
 
 def opentestfile(fname):
     f = open (fname, "r")
-    x= f.read () 
+    x= f.read() 
     f.close ()
     return x
-
 
 x = opentestfile(fname)
 x = "Mr. Johnny Deep was loved by Emily J. Jenkino but not Jane Terrance."
 def findname():
-    '''
-    m= re.search(r".[^ab]+",x) #bcdef
-    m= re.search(r"[a-f]+",x) #abcdef
-    m = re.findall("[A-Z][a-z]+",x)  #gets all capitalized words
-    '''
     m = re.findall("[A-Z][a-z|-]+",x) #gets all capitalizes words
-    z = re.findall("[A-Z][a-z|'|.|]+ [A-Z][a-z|'|\.]+",x)
-    m = re.findall("[A-Z][a-z|'|.|]+ [A-Z][a-z|'|\.]+ [A-Z][a-z|'|\.]+",x)
-    print z
-    print m
-    #build dictionary or list with names (dictionary would be more efficient probably
-    matches=[y for y in m if y in names]
+    z = re.findall("[A-Z][a-z|'|.|]+ [A-Z][a-z|'|\.]+",x) #
+   # matches = re.findall("[A-Z][a-z|'|.|]+ [A-Z][a-z|'|\.]+ [A-Z][a-z|'|\.]+",x)
+   
+    matches = re.findall("([A-Z][a-z]+[-]?[A-Z]?[a-z]+[\']?[s]?[\s]?)+",x)
+   
     numbnames={}
     for name in matches:
         if name in numbnames:
             numbnames[name]+=1
         else:
             numbnames[name]=1
-    
+    print matches
+    return "DONE SEARCHING"
+
+'''
     namesfound= []
     for name in m:
-       namesfound.append(name)
+        if re.search("[\"|\']?(?<=Mr\.|Mrs\.|Ms\.|)([\s]?[A-Z][a-z]+[-]?[A-Z]?[a-z]+[']?[s]?)+",name)!= None:
+           # re.sub("[\"|\']?(?<=Mr\.|Mrs\.|Ms\.|)([\s]?[A-Z][a-z]+[-]?[A-Z]?[a-z]+[']?[s]?)+",name,"")
+            namesfound.append(name)
+    namesfound.append(name)
+
+
+   
     print namesfound
-    #return numbnames
-    #return matches
+'''
+
     
 
 
