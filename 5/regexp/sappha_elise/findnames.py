c9a031faa497b244b9c5aecbec67a3a851c4a9e4
sappha.omeara <sappha.omeara@gmail.com>
2014-10-03 11:45:25 -0400

    titles and names

--- 5/regexp/sappha_elise/findnames.py
+++ 5/regexp/sappha_elise/findnames.py
@@ -6,5 +6,12 @@ data.close()
 
 match_twoNames = re.findall(r'([A-Z][a-z]+ [A-Z][a-z]+)', text)
 match_titles = re.findall(r'([A-Z][a-z]+\. [A-Z][a-z]*)', text)
-print match_twoNames
-print match_titles
+##print match_twoNames
+##print match_titles
+
+names = []
+for name in match_twoNames:
+    names.append(name)
+for name in match_titles:
+    names.append(name)
+print names
