ba188a024c8c84112b4fd52f570eab064dd6f23c
sappha.omeara <sappha.omeara@gmail.com>
2014-10-03 11:42:21 -0400

    more work

--- 5/regexp/sappha_elise/findnames.py
+++ 5/regexp/sappha_elise/findnames.py
@@ -4,5 +4,7 @@ data = open("textnames", 'r')
 text = data.read()
 data.close()
 
-matchNames = re.findall(r'([A-Z][a-z]+ [A-Z][a-z]+)', text)
-print matchNames
+match_twoNames = re.findall(r'([A-Z][a-z]+ [A-Z][a-z]+)', text)
+match_titles = re.findall(r'([A-Z][a-z]+\. [A-Z][a-z]*)', text)
+print match_twoNames
+print match_titles
