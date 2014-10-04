f90cbbb09b0ff6521ff99093e5d72062e525c3e8
Nadia <nadiasaleh26@gmail.com>
2014-10-04 15:04:27 -0400

    modified regex, added time filter

--- 7/regexp/jstrauss_nsaleh/namefinder.py
+++ 7/regexp/jstrauss_nsaleh/namefinder.py
@@ -4,6 +4,12 @@
 
 import re
 
+
+
+timeIndicators = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+
+
 def readText(filename):
 	# reads a file and returns it as a string of content
 	instream = open(filename,'r')
@@ -11,6 +17,25 @@ def readText(filename):
 	instream.close()
 	return text
 
+<<<<<<< HEAD
+def listPossibleNames(filename):
+	# uses a regular expression to pass the text through the first layer of filtration
+	# any group of 2, or 3 subsequent words beginning with caps will be added
+	text = readText(filename)
+	names = []
+	for m in re.finditer(r"(([A-Z][a-z]+\s?)(-?)){2,}", text):
+                possiblename = m.group(0)
+                if possiblename[-1] == " ":
+                        possiblename = possiblename[:-1]
+                names.append(possiblename)
+
+                #names.append( '%02d-%02d: %s' % (m.start(), m.end(), m.group(0)) ) ##why the start and end?
+	print names
+        print "\n"
+        return names # returns a list 
+
+def filterTwo(names):
+=======
 def makeList(filename):
 	# turns the raw census databases of names into lists
 	instream = open(filename,'r')
@@ -41,6 +66,7 @@ def processSingleNames(text):
 
 def filterTwo():
 	names = listPossibleNames("stateoftheunion.txt")
+>>>>>>> 810fd033f1fb8f1f764a9be6db9c4f7a5cb9b4aa
 	justthenames = []
 	for name in names:
 		justthenames.append( name[name.find(":")+2:] )
@@ -50,9 +76,41 @@ def filterTwo():
 	#possibleNames.group(0)
 	#return possibleNames
 
+<<<<<<< HEAD
+def filterThree (possiblenames):
+        newList = []
+        for name in possiblenames:
+                hasTimeWord = False
+                eachword = name.split(" ")
+                for word in eachword:
+                        if  word in timeIndicators:
+                                hasTimeWord = True
+                if not hasTimeWord:
+                        newList.append(name)
+                                
+        print newList
+        return newList
+        
+
+def findNames(filename):
+	possiblenames = listPossibleNames(filename)
+	#filterTwo(possiblenames)
+        filterThree(possiblenames)
+
 if __name__=="__main__":
+
+	findNames("test.txt")
+
+
+=======
+if __name__=="__main__":
+>>>>>>> 810fd033f1fb8f1f764a9be6db9c4f7a5cb9b4aa
 	#filename = raw_input("What file do you want to find people's names in?\n")
 	#try:
 	print processTripleNames(readText(str("test.txt")))
 	#except:
+<<<<<<< HEAD
 	#	print "Filename invalid. Please try again."
+=======
+	#	print "Filename invalid. Please try again."
+>>>>>>> 810fd033f1fb8f1f764a9be6db9c4f7a5cb9b4aa
