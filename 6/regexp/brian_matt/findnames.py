f901d0c78126cbe44171447ec0b968b9e1ba693a
Matthew Cook <mcookhome@gmail.com>
2014-10-04 18:24:45 -0400

    working well now

--- 6/regexp/brian_matt/findnames.py
+++ 6/regexp/brian_matt/findnames.py
@@ -1,41 +1,47 @@
-import re
+import re, pickle,sys
 
-f = open('in.txt','r')
+
+filename = sys.argv[1]
+
+f = open(filename+'.txt','r')
 g = f.read()
 f.close()
 
+h = open('diction.txt','rb')
+d= pickle.load(h)
+h.close()
 
-def makedict():
-    s= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    words = []
-    for n in s:
-        t = open("dict/" + n + " Words.csv")
-        r = t.readlines()
-        for i in xrange(len(r)-1):
-            r[i] = r[i][:-1]
-        words.extend(r)
-        
-        t.close()
-   
-        
-   
-    return words
 
-d= makedict()
+def notname(lis):
+    x=0
+    for sub in lis:
+        if sub in d:
+            #print sub
+            pass
+            x=x+1
+            #print x
+        if x==2: 
+            return True
+    return False
+
+def stringify(L):
+    s = ""
+    for n in L:
+        for sub in n:
+            s = s+" "+sub
+        s=s+","
+    return s[:-1]
 
 def findnames():
     p = re.compile('[A-Z][a-z]+\.*(?:\s[A-Z][a-z]+)+')
     L=p.findall(g)
+    x=0
     for i in xrange(len(L)):
         L[i] = L[i].replace('\n',' ')
         L[i] = L[i].lower().split(" ")
-    for n in L:
-        for sub in n:
-            if sub in d:
-                pass
-            else:
-                break
-                
+    print stringify(L)
+
+    L[:] = [ o for o in L if notname(o)==False]
     return L  
 
-print findnames()
+print stringify(findnames())
---
+++ 6/regexp/brian_matt/makedict.py
@@ -0,0 +1,22 @@
+import pickle
+
+def makedict():
+    s= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    words = []
+    for n in s:
+        t = open("dict/" + n + " Words.csv")
+        r = t.readlines()
+        for i in xrange(len(r)-1):
+            r[i] = r[i][:-1]
+        words.extend(r)
+        
+        t.close()
+    
+    return words
+
+
+
+wr = open('diction.txt', 'wb')
+d= makedict()
+pickle.dump(d,wr)
+wr.close()
