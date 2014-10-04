c5917e818cda1afc570cd372fe667c0a87cb603a
Richard Zhan <lilfirebolt@gmail.com>
2014-10-04 14:49:50 -0400

    Vector does not work

--- 7/regexp/richard-max/analysis.py
+++ 7/regexp/richard-max/analysis.py
@@ -17,6 +17,12 @@ def last_first(text):
     s = re.findall(r'[A-Z]\w+,\s[A-Z]\w+', text)
     return s
 
+def name_vector_filter(text):
+    data = []
+    
+    
+    
+    print data
 def validate_title(names,num,test):
     #print names + "\n\n"
     return names[num][0]==test
@@ -51,12 +57,57 @@ class names(unittest.TestCase):
         
 if __name__=="__main__":
     #unittest.main()
+    #So I compared the vectors of the dictionary and the list of male names
+    #, and they are practically identical, so I cannot use a vector to distinguish
+    # English words from English names. Oh well.
+    """
+    s = 'abcdefghijklmnopqrstuvwxyz'
+    import math
+
+
+    d = open('words.txt').readlines()
+    datad = [x.rstrip() for x in d]
+    dd={}
+    for x in s:
+        dd[x]=0
+    for x in datad:
+        for y in x:
+            if y in dd:
+                dd[y]+=1
+    for x in xrange(len(dd)):
+        print list(dd.viewkeys())[x] + " " + str(list(dd.viewvalues())[x])
+    vector = math.sqrt(sum([x*x for x in dd.viewvalues()]))
+    print vector/851.643234
+
+    
+    nm = open('males.txt').readlines()
+    datam = [x.rstrip().lower() for x in nm]
+    dm={}
+    for x in s:
+        dm[x]=0
+    for x in datam:
+        for y in x:
+            dm[y]+=1
+    for x in xrange(len(dm)):
+        print list(dm.viewkeys())[x] + " " + str(list(dm.viewvalues())[x])
+    vector = math.sqrt(sum([x*x for x in dm.viewvalues()]))
+    print vector#569
+            
+    nf = open('females.txt').readlines()
+    dataf = [x.rstrip().lower() for x in nf]
+    df={}
+    for x in s:
+        df[x]=0
+    for x in dataf:
+        for y in x:
+            df[y]+=1
+
+    """
+            
     f = open('data.txt', 'r')
     data = ''
     for x in f:
         data += x
     f.close()
     captures = [x[0] for x in first_last(data)]
-#    first_last(data)
-    #print data
     print captures
