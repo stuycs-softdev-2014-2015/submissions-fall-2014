04b6737497cd74c1ad7ad7d5f6ac4abdb4a6f7c0
Natan <natan.zamansky@gmail.com>
2014-10-04 17:23:32 -0400

    Our work

---
+++ 6/regexp/benedict-natan/nameFind.py
@@ -0,0 +1,32 @@
+import re
+
+fi = open("nick.txt","r")
+content = fi.read()
+fi.close()
+
+#Start at 16484
+fi2 = open("/usr/share/dict/words", "r")
+dictionary = fi2.read()
+words=dictionary.split("\n")
+words=words[16484:]
+fi2.close()
+
+for i in range(len(words)):
+	words[i]=words[i].upper()
+
+firstPass = re.findall("[A-Z][A-Za-z'\-]*\s[A-Z][A-Za-z'\-]*", content)
+
+secondPass = []
+for word in firstPass:
+	names=word.split(" ")
+	check=True
+	for name in names:
+		if name.upper() in words:
+			check=False
+	if check:
+		secondPass.append(word)
+
+thirdPass = list(set(secondPass))
+
+for word in thirdPass:
+    print word + "\n"
