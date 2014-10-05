a8fa746b954c2cf7be5fa486caf048e7e146c080
jasonluX13 <jasonluX13@gmail.com>
2014-10-04 21:19:29 -0400

    Removed duplicates from output

--- 5/regexp/jason_fawn/regex.py
+++ 5/regexp/jason_fawn/regex.py
@@ -1,5 +1,6 @@
 import re
 
+
 data = open("15414.txt", "r")
 text = data.read()
 data.close()
@@ -10,24 +11,40 @@ for line in fname:
     firstnames.append(line.split()[0].capitalize())
 fname.close()
 
+t = open("titles.txt","r")
+titles = t.read()
+t.close()
+
+for line in titles:
+	firstnames.append(line)
+
+'''
 lname = open("lastnames.txt", "r")
 last = lname.read()
 lname.close()
-
-#firstnames = re.findall(r'[A-Z][a-z]', first)
+'''
+def cleanUp(list):
+    newlist = []
+    for thing in list:
+        if thing not in newlist: 
+            newlist.append(thing)    
+    return newlist
 
 names = []
 def findNames(s):
     matchNames = re.findall(r'([A-Z][a-z.-]+ [A-Z][a-z.-]+)', s)
     ifName(matchNames)
     matchNames = re.findall(r'([A-Z][a-z.-]+ [A-Z][a-z.-]+ [A-Z][a-z.-]+)', s)
-    ifName(matchNames)     
-    print names
+    ifName(matchNames)    
+    print(cleanUp(names))
+    #print (names)
 
 def ifName(l):
     for name in l:
         if name.split()[0] in firstnames:
             names.append(name)
 
+
+	
 if __name__=="__main__":
     findNames(text)
