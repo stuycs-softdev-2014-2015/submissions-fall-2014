f8e9dbaf6f244b50a226a873d84d997b77f577d6
deannataylor <dmtaylor@gmail.com>
2014-10-05 15:13:41 -0400

    added bible, tried to fix loop, did a lot of testing but what

--- 6/regexp/thomas-deanna/NameFind.py
+++ 6/regexp/thomas-deanna/NameFind.py
@@ -3,7 +3,7 @@ import re
 def FindNames():
 
     #getting possible names from string
-    file = open('names.txt')
+    file = open('Bible.txt')
     regex=re.compile("[A-Z][a-z]+[^0-9][^\W][\s]")
     names = regex.findall(file.read())
     file.close()
@@ -12,17 +12,28 @@ def FindNames():
     file = open('popnames.txt')
     regex2=re.compile("[A-Z]+")
     popnames  = regex2.findall(file.read())
-    print len(popnames)
+    """print len(names)"""
     file.close()
+
+    #Testing where we lose names
+    print len(names)
+
     for n in range(len(popnames)):
         popnames[n] = popnames[n].lower().capitalize()
-    final = []
-    for i in range(len(names)):
-        if names[i] in popnames:
-            final.append(names[i])
+    i = 0
+    print len(names)
+
+    while i < len(names):
+        if not names[i] in popnames:
+            names.remove(names[i])
+        else:
+            i+= 1
+
     #print
-    print final
-    
+    """print popnames"""
+    print len(names)
+    print names
+
         
 if __name__ == "__main__":
     FindNames()
