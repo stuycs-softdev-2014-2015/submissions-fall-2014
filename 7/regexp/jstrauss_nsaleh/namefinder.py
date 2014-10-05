b8ed640f056002337b18649bf00f49dc33b153e7
Nadia <nadiasaleh26@gmail.com>
2014-10-05 15:29:34 -0400

    duplicate check

--- 7/regexp/jstrauss_nsaleh/namefinder.py
+++ 7/regexp/jstrauss_nsaleh/namefinder.py
@@ -7,7 +7,7 @@ import re
 def readText(filename):
 	# reads a file and returns it as a string of content
 	instream = open(filename,'r')
-	text = instream.read().replace("\n"," ")
+	text = instream.read().replace("\n"," ").replace("\r"," ")
 	instream.close()
 	return text
 
@@ -33,34 +33,37 @@ def filterNames (possiblenames):
         femaleNames = makeList("femalenames.txt")
         maleNames = makeList("malenames.txt")
         surnames = makeList("surnames.txt")
-
         firstNames = femaleNames + maleNames
+
         names = []
 
         for name in possiblenames:
-                eachword = name.split(" ")
-                firstName = eachword[0].upper()
-                secondName = eachword[1].upper()
-
-                if firstName in firstNames:
+                if not name in names and nameCheck(name,firstNames,surnames):
                         names.append(name)
-                elif secondName in firstNames or secondName in surnames:
-                        names.append(name)
-                elif len(eachword) == 3:
-                        thirdName = eachword[2].upper()
-                        if thirdName in surnames:
-                                names.append(name)
         return names
+
+def nameCheck(name, firsts, lasts):
+        eachword = name.split(" ")
+        firstName = eachword[0].upper()
+        secondName = eachword[1].upper()
+        if firstName in firsts:
+                return True
+        elif secondName in firsts or secondName in lasts:
+                return True
+        elif len(eachword) == 3:
+                thirdName = eachword[2].upper()
+                return True
+        return False
 
 
 def findNames(filename):
         filetext = readText(filename)
         possiblenames = findPossibleNames(filetext)
-        filterNames(possiblenames)
+        print(filterNames(possiblenames))
         
 
 if __name__=="__main__":
-        findNames("stateoftheunion.txt")
+        findNames("prideandprejudice.txt")
 	#filename = raw_input("What file do you want to find people's names in?\n")
 	#try:
 	#print processTripleNames(readText(str("test.txt")))
