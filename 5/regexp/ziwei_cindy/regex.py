caeb45bc8637eb87a04246c8ff430c09d7c818d2
Izui Kanae <Calvingit config --global user.email Calvin@yuugu.regit config --global user.email Calvin>
2014-10-05 12:31:48 -0400

    works with titles and last name

--- 5/regexp/ziwei_cindy/regex.py
+++ 5/regexp/ziwei_cindy/regex.py
@@ -1,24 +1,34 @@
 import re
 
-comwords = ["My","We","You","He","She","Her","His","Mr","Mrs","Ms","Dr","The","Then","After","Before","Later","Next","Never","Perhaps","During","While","So","No","Not","Yes","But","To","Do","For","From","Who","What","When","Where","Why","How","Would","Was","Had","With","Their","They","There","This","That","Those","These","Well","By","On","At","As","An","Of","It","Is","If","In","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","January","February","March","July","August","September","October","November","December"]
+comwords = open("filter.txt", "r").read().split("\n")
 
+def namefilter(names):
+    for x in names:
+        if (x[0] in comwords or x[1] in comwords):
+            x[0], x[1] = x[1], ''
+    return [x for x in names if x != ['','']]
+
+        
 def findNames(f):
-    file = open(f, "r")
-    file = file.read()
-    print file
+    filestream = open(f, "r")
+    file = filestream.read()
+    filestream.close()
 
-    #Find all words with capitalized first letters
-    firstrun = re.findall(r'([A-Z][a-z]+)',file,re.S)
+    #Find all words with capitalized first letters and followed by another captitalized word, optional
+    #result is returned into tuple form
+    firstrun = re.findall(r"([A-Z][a-z\-]+)( [A-Z][a-zA-Z\-]+)?",file,re.S)
+    #Filtering from list and covert the tuples into lists. and then conc together
+    firstrun = namefilter([list(x) for x in firstrun])
+    firstrun = [(x[0] + x[1] if x[1] != None else x[0]) for x in firstrun]
 
     #Put all words in a dictionary with the frequency of which they appear
     #If the word appears in the list of common words, do not add it
     secrun = {}
     for n in firstrun:
-        if (n not in comwords):
-            if (n not in secrun):
-                secrun[n] = 1
-            else:
-                secrun[n]+=1
+        if (n not in secrun):
+            secrun[n] = 1
+        else:
+            secrun[n] += 1
 
     #Reverse names and frequency for next step
     #looks like -> {1:"Yesterday, That", 100:"It", 124: "The", ...}
@@ -27,17 +37,13 @@ def findNames(f):
         if (v not in names):
             names[v] = [k]
         else:
-            names[v].append(k);
-
-    #Takes out the least common words found 
-    #(so sentence starters or uncommon names are removed)
+            names[v].append(k)
     size = len(names)*4/5
     i = 0
     while( len(names) > size ):
         if i in names:
             del names[i]
         i += 1
-    #print names
 
     #This just puts all the names in a final list
     namesfinal = []
@@ -46,4 +52,5 @@ def findNames(f):
             namesfinal.append(n)
     print namesfinal
 
-findNames("TheLonelyUnicorn.txt")
+if __name__ == "__main__":
+    findNames("TheLonelyUnicorn.txt")
