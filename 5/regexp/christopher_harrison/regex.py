be1ab516568db03fd9d074165149709b02e42e65
Applesauce136 <harrison.chiu.136@gmail.com>
2014-10-05 13:19:30 -0400

    Merged the three different extraction procedures into one.

--- 5/regexp/christopher_harrison/regex.py
+++ 5/regexp/christopher_harrison/regex.py
@@ -1,50 +1,48 @@
 import re
 
+def extract_names(string):
+    '''Takes a string and returns a list of all names in the string.'''
+    names = []
+    for word in string.split():
+        word = letters_only(word)
+        if is_name(word):
+            names.append(word)
+    return names
+
+def letters_only(string):
+    '''Takes a string and returns a new string comprising all the letters of the old string.'''
+    result = []
+    for char in string:
+        if re_letter.match(char):
+            result.append(char)
+    return "".join(result)
+
+def is_name(string):
+    '''Takes a string and returns True if the string is a name; and false otherwise.'''
+    return ( (re_caps.match(string)) and
+             ( (string.upper() in d_names) or
+               not (string.upper() in d_words)))
+
 def fill(dicti, filename):
     for word in open(filename).read().split():
         dicti[word] = 0
     return dicti
 
+#----------------------------------------------------------------
+
 d_words = {}
 fill(d_words, "words.txt")
-
 d_names = {}
 fill(d_names, "names.txt")
 
-def extract_names_regex(string):
-    result = []
-    re_caps = re.compile('^[A-Z][a-z]+.$')
-    for word in string.split():
-        if re_caps.match(word):
-            result.append(word)
-    return result
-
-def extract_names_names(string, d_names):
-    '''Matches each word against a list of names, and includes the word if it is found.'''
-    result = []
-    for word in string.split():
-        print word
-        if word in d_names:
-            result.append(word)
-    return result
-        
-
-def extract_names_words(string, d_words):
-    '''Matches each word against a list of words, and includes the word if it is NOT found.'''
-    result = []
-    for word in string.split():
-        if not(word in d_words):
-            result.append(word)
-    return result
+re_letter = re.compile("([a-z])|([A-Z])")
+re_caps = re.compile('^[A-Z][a-z]+.$')
 
 def test(filename):
     f_read = open("test_files/" + filename + ".txt", "r")
-    f_write_regex = open("test_results/" + filename + "_regex", "w")
-    f_write_names = open("test_results/" + filename + "_names", "w")
-    f_write_words = open("test_results/" + filename + "_words", "w")
-
-    f_write_regex.write(str(extract_names_regex(f_read.read())))
-    f_write_names.write(str(extract_names_names(f_read.read(), d_names)))
-    f_write_words.write(str(extract_names_words(f_read.read(), d_words)))
+    f_write = open("test_results/" + filename + ".nam", "w")
+    f_write.write( "\n".join(extract_names( f_read.read() )))
+    f_read.close()
+    f_write.close()
 
 test("secret_service")
