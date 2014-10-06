3dcddf87fd3dff2d2c7127c3e1a21b3e16020958
l-tn3314 <l.tn3314@gmail.com>
2014-10-06 01:19:10 -0400

    more cleaning up

--- 5/regexp/jenny_tina/regex.py
+++ 5/regexp/jenny_tina/regex.py
@@ -1,6 +1,7 @@
 import re
 
 f = open('text.txt', 'r')
+#f = open('text2.txt', 'r')
 rawtext = f.read() #test is a string
 f.close()
 
@@ -19,7 +20,10 @@ while i < len(words):
 reg = re.compile('[A-Z]([a-z|\-|\'])+ [A-Z]([a-z|\-|\'])+')
 names = []
 
-dict = {}
+#error once the '#' is removed
+#Doesn't always work
+nonNames = ["Dear", "College", "University", "Street", "Avenue", "Boulevard", "School", "City", "He ", "His ", "She ", "Her ", "The ", "And ", "But", "This "]#, "So ", "Yet "]
+
 j = 0
 while j < len(pairs):
     if ( reg.match(pairs[j]) != None and pairs[j] not in names):
@@ -28,9 +32,17 @@ while j < len(pairs):
 
 k = 0
 while k < len(names):
-    if ((names[k] + "").find("Dear") > -1 or (names[k] + "").find("College") > -1
-        or (names[k] + "").find("University") > -1):
-        names.pop(k)
+    for each in nonNames:
+        if names[k].find(each) > -1:
+    #if (names[k].find("Dear") > -1 or names[k].find("College") > -1
+        #or names[k].find("University") > -1 or names[k].find("Street") > -1
+        #or names[k].find("Avenue") > -1 or names[k].find("Boulevarrd") > -1
+        #or names[k].find("School") > -1 or names[k].find("City") > -1
+        #or names[k].find("He ") > -1 or names[k].find("His ") > -1
+        #or names[k].find("She ") > -1 or names[k].find("Her ") > -1
+        #or names[k].find("The ") > -1 or names[k].find("And ") > -1
+        #or names[k].find("But ") > -1 ):
+            names.pop(k)
     k = k + 1
 
 
