f670faaed80a74dc6b3d20c3da5d7393275fb2ff
Michael Huang <mikester591@gmail.com>
2014-10-05 20:47:53 -0400

    Added exceptions

--- 6/regexp/sadman-michael/regex.py
+++ 6/regexp/sadman-michael/regex.py
@@ -6,15 +6,26 @@ strbook = open("tale.txt", "r")
 strbook1 = strbook.read()
 strbook.close()
 
+ex=open("title.txt","r")
+ex1=ex.readlines()
+ex.close()
+
+
 
 results = re.findall("([A-Z][a-z]+)\s([A-Z][a-z]+)", strbook1)
 
 names = ''
-
+def notlist(x,d):
+    for l in x:
+        if d  in l:
+            return False
+    return True
 for x in results:
-    #print x[0]
-    print x[0]#!= "Our":
-#        names.append(results[x])
-
+    if notlist(ex1,x[0])== True:
+        names+=(x[0]+"-"+x[1]+" ")
+               
 print names
 
+
+
+    
