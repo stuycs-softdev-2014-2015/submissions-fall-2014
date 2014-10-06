1d44bae9e76e6ae91801bfbc15484fb54f6f6351
Zabari <zabari.daniel@Gmail.com>
2014-10-06 01:40:01 -0400

    clean up
    interesting results

--- 6/regexp/sophie_daniel/namefind.py
+++ 6/regexp/sophie_daniel/namefind.py
@@ -1,38 +1,19 @@
 import re
-f=open("first.txt",'r')
-s=f.read()
-f.close()
-#p=re.compile("(([A-Z][a-z]*)\s([A-Z][a-z]*))")
-#p=re.compile("([A-Z][a-z]*\s*){2,}")
-p=re.compile("([A-Z][a-z]*)\s([A-Z][a-z]*)")
-pre= p.findall(s)  
-r=re.compile("((?<!\.\s)(?<!\.\s\s)(?<!\.\s\s\")(?<!\.\s\")(?<!\"\s)(?<!\"\s\s)(?<!\")[A-Z][a-z]+)") 
-l=r.findall(s)
-for f in pre:
-    for se in f:
-        l.append(se)
-d={name: s.count(name) for name in l}
-#for name in r.findall(s):
-#    print name+":" +str(s.count(name))
-#print d
-print "-------------------"
-
-
-def namefind(filename):
-    f=open(filename,'r')
-    s = f.read()
+def nameFind(fil):
+    f=open(fil,'r')
+    s=f.read()
     f.close()
+    #p=re.compile("(([A-Z][a-z]*)\s([A-Z][a-z]*))")
+    #p=re.compile("([A-Z][a-z]*\s*){2,}")
     p=re.compile("([A-Z][a-z]*)\s([A-Z][a-z]*)")
-    pre= p.findall(s)
-    exp = "((?<!\.\s)(?<!\.\s\s)(?<!\.\s\s\")(?<!\.\s\")(?<!\.\"\s)(?<!\.\"\s\s)(?<!\")[A-Z][a-z]+)"
-    r=re.compile(exp)
+    pre= p.findall(s)  
+    r=re.compile('((?<![\.\!\?\"])(?<![\.\!\?\"\:\;]\s)(?<![\.\!\?\"\;\:]\s\s)[A-Z][a-z]+)')
     l=r.findall(s)
     for f in pre:
         for se in f:
             l.append(se)
     d={name: s.count(name) for name in l}
-    print d
-
-
-    
-namefind("first.txt")
+    #for name in r.findall(s):
+    #    print name+":" +str(s.count(name))
+    return d
+print nameFind('first.txt')
