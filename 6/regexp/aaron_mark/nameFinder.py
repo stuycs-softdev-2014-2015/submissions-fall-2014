2edd8215326ad32ef343b2fa9c3bc122e6661424
Aaron Mortenson <aamortenson1@gmail.com>
2014-10-05 23:26:18 -0400

    Update nameFinder.py

--- 6/regexp/aaron_mark/nameFinder.py
+++ 6/regexp/aaron_mark/nameFinder.py
@@ -1,18 +1,14 @@
 
 import re
 
-reg1 = "(([DMS][ris]{1,3}\.? )?([A-Z][a-z]*)([\s-][A-Z][a-z]*))"
-
-
-reg = "(([DMS][ris]{1,3}\.? )?((([A-Z][a-z]*)([\s-][A-Z][a-z]*))|([A-Z][a-z]*([\s-]))))"
-
-#ret = re.findall(reg, "Mark Norwich hey Dr. Mark  there Mr. Cooper Weaver hi how are Mr. you Dr. Sam Mortenson")
+reg = "((([DMS][ris]{1,3}\.? )?([A-Z]([a-z]*|\.)){1}([\s-][A-Z]([a-z]*|\.)){0,1}([\s-][A-Z][a-z]+){1}((,?[\s-][JjSs]r.)|([\s-][XIV]+)){0,1})|([DMS][ris]{1,3}\.?[\s-][A-Z][a-z]*))"
 
 
 def run(filename):
-    file  = open("filename", "r")
+    file  = open(filename, "r")
     ret = file.read()
     ret = re.findall(reg, ret )
     for x in ret:
         print x[0]
+
 run("words.txt")
