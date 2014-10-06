0157c8e64552a27dabb21fbd5c86fe22c282e9cf
Tyler Wang <twang.123@live.com>
2014-10-05 22:48:26 -0400

    finds all caps

--- 7/regexp/Steven-Tianshi/regexp.py
+++ 7/regexp/Steven-Tianshi/regexp.py
@@ -10,11 +10,12 @@ def check(s):
             return True
     return False
 
+capitals = re.findall("([A-Z][a-z]*)",s)
 stuff = re.findall("(Mr\.|Ms\.|Mrs\.|Mr|Ms|Mrs) ([A-Z][a-z]+ [A-Z][a-z]+|[A-Z][a-z]+)",s)
 stuff2 = re.findall("[^\.\?!] ([A-Z][a-z]+ [A-Z][a-z]+|[A-Z][a-z]+)",s)
 exempt = ["Mr.","Ms.","Mrs.","Mr","Mrs","Ms"]
 [stuff2.remove(x) for x in stuff2 if (check(x))]
 stuff.extend(stuff2)
-print stuff
-
+#print stuff
+print capitals
 
