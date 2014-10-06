97b9b7528d943065c771620f5b0d4800b4235400
StevenZabolotny <stevezabolotny@gmail.com>
2014-10-05 22:50:55 -0400

    Added list of names, python file to process it, and compare to words found.

--- 7/regexp/Steven-Tianshi/regexp.py
+++ 7/regexp/Steven-Tianshi/regexp.py
@@ -1,7 +1,12 @@
 import re
 
-f = open("metamorphosis.txt")
+f = open("prideandprejudice.txt")
 s = f.read()
+f.close()
+
+f = open("names.txt")
+names = f.read()
+namesL = names.split(",")
 f.close()
 
 def check(s):
@@ -10,11 +15,20 @@ def check(s):
             return True
     return False
 
+def checkNames(L):
+    ret = []
+    for s in L:
+        for name in namesL:
+            if (s == name):
+                ret.append(s)
+    return ret
+
 stuff = re.findall("(Mr\.|Ms\.|Mrs\.|Mr|Ms|Mrs) ([A-Z][a-z]+ [A-Z][a-z]+|[A-Z][a-z]+)",s)
 stuff2 = re.findall("[^\.\?!] ([A-Z][a-z]+ [A-Z][a-z]+|[A-Z][a-z]+)",s)
 exempt = ["Mr.","Ms.","Mrs.","Mr","Mrs","Ms"]
 [stuff2.remove(x) for x in stuff2 if (check(x))]
 stuff.extend(stuff2)
 print stuff
+print checkNames(stuff)
 
 
