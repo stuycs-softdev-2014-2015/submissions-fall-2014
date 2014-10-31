34e28e33f3cd9aca9a9d3c0344757832512b57e3
vanessa <yu100997@gmail.com>
2014-10-31 11:49:38 -0400

    tried to do things, not sure if worked

--- 5/login/vanessa_harrison/app.py
+++ 5/login/vanessa_harrison/app.py
@@ -1,4 +1,4 @@
-from flask import *
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 
 '''
 login
@@ -12,26 +12,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    if session['username'] == None:
-        return redirect(url_for("login"))
+    if "username" in session:
+        return "YOURE LOGGED IN AS %s" %escape(session["username"])
     else:
-        return "YOURE LOGGED IN"
+        return "this is the home page which means you arent logged in"
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-    if request.method == "POST":
-        session['username'] = request.form['username']
+    if "username" in session:
+        return "youre already logged in as %s" %escape(session["username"])
+    elif request.method == "POST":
+        session["username"] = request.form["username"]
+        return redirect(url_for(index))
     return render_template("login.html")
-
+    
 @app.route("/logout")
 def logout():
+    if "username" in session:
+        return render_template("logout.html")
+    elif request.args.get("logout") == "logout":
+        session.pop("username", None)
+        return redirect(url_for(index))
+    else:
+        return "water u dongyou arent in"
+    '''
     if request.args.get("logout")=="logout":
         session.pop('username', None)
-        return redirect(url_for("index"))
+        return redirect(url_for(index))
     elif session['username'] != None:
         return render_template("logout.html")
     else:
         return "water u doing youa rent in"
+    '''
 
 if __name__ == "__main__":
     app.secret_key = "asdf"
