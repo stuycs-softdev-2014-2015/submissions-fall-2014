eaaee9220953703fe61841f1deefbf2485ea7310
Abrar Ahmed <abrarahmed257@gmail.com>
2014-10-31 11:44:25 -0400

    more changes

--- 5/login/abrar_elise/app.py
+++ 5/login/abrar_elise/app.py
@@ -5,11 +5,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("login.html")
+    return render_template("home.html")
 
-@app.route("/form", methods=["GET", "POST"])
-def form():
+@app.route("/pform", methods=["GET", "POST"])
+def pform():
+    if request.method == "GET":
+        return render_template("pform.html")
+    else:
+        uname = request.form['uname']
+        pword = request.form['pass']
+        print "uname: " + uname + "\npass" + pword
 
+
+                              
 if __name__=="__main__":
     app.debug=True
     app.run(host="0.0.0.0", port=1847)
