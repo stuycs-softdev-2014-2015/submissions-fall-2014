3b8abf2ab88019aea626f0a0f88db52946cbd4a2
Cindy <cindyofwu@yahoo.com>
2014-10-31 11:24:39 -0400

    folder

---
+++ 5/login/adriel_cindy/mongo.py
@@ -0,0 +1,17 @@
+from pymongo import Connection
+
+conn = Connection()
+
+db = conn['Login']
+conn.drop_database('Login');
+def add_user(u, pw):
+    db.users.insert({'user':u, 'password':pw})
+
+def update_user(u, i):
+    db.users.update({'user':u}, {'$set':{'info':i}})
+
+add_user("hi","bye")
+
+res = db.users.find({},{'_id':False})
+for r in res:
+    print r
