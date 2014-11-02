0417d200149f634ced49c381f825b925bee00dec
justinstrauss <justinianstrauss@gmail.com>
2014-11-02 11:53:01 -0500

    coming along nicely

--- 7/mongoproj/jstrauss_dtsui/app.py
+++ 7/mongoproj/jstrauss_dtsui/app.py
@@ -3,14 +3,13 @@
 # MongoDB Project
 
 import random
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from pymongo import Connection
-#let this be the main file
 
 app = Flask(__name__)
-app.secret_key = "don't store this on github"
 
 @app.route('/', methods=["POST","GET"])
+@app.route('/index', methods=["POST","GET"])
 def index():
     # if request.method == "POST":
     #     form = request.form
@@ -32,29 +31,31 @@ def index():
     # res = db.jsdt.find({})
     # info = [x for x in res]
     # print info
-    return render_template("index.html")
+    return render_template("index.html",user=None)
+
+@app.route("/login")
+def login():
+    return render_template("login.html",user=None)
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html",user=None)
 
 @app.route("/profile")
 def profile():
-    return render_template("profile.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+    return render_template("profile.html",user=None)
 
 @app.route("/contacts")
 def contacts():
-    return render_template("contacts.html")
+    return render_template("contacts.html",user=None)
 
 
 
 if __name__ == '__main__':
     # conn = Connection()
     # db = conn['jsdt']
+    app.secret_key = "don't store this on github"
     app.debug = True
     app.run(host='0.0.0.0')
     
