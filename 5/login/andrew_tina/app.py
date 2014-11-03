7657646c4c5826e85fa8fab1275dbf7d89d6d42c
l-tn3314 <l.tn3314@gmail.com>
2014-11-02 20:32:09 -0500

    initial stuff

---
+++ 5/login/andrew_tina/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
