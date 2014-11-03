c52d18bed0544c55a542ab6e0b84eb694833b4f8
abrarahmed56 <abrarahmed257@gmail.com>
2014-11-03 06:30:18 -0500

    mongo stuff

--- 5/login/abrar_elise/app.py
+++ 5/login/abrar_elise/app.py
@@ -1,23 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import Connection
 
 app = Flask(__name__)
+conn = Connection()
+db = conn ['aaez']
 
 @app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/pform", methods=["GET", "POST"])
-def pform():
-    if request.method == "GET":
-        return render_template("pform.html")
+def index():
+    button = request.args.get("sub", None)
+    username = request.args.get("username", None)
+    passw = request.args.get("password", None)
+    if button == "Register":
+        if db.users.find_one ( { 'name' : username } ) == None:
+            db.users.insert ( { 'name': username, 'pword': passw } )
+            return "<h1>Thanks for joining!</h1>"
+        else:
+            return "<h1>Please select an available username</h1>"
+    elif button == "Login":
+        loginfo = { 'name': username, 'pword': passw }
+        if db.users.find_one ( { 'name' : username , 'pword' : passw } ) != None:
+            return "correct login info"
+        return "incorrect login info"
     else:
-        uname = request.form['uname']
-        pword = request.form['pass']
-        print "uname: " + uname + "\npass" + pword
+        return render_template("index.html")
 
 
-                              
 if __name__=="__main__":
-    app.debug=True
-    app.run(host="0.0.0.0", port=1847)
+    app.debug = True
+    app.run()
+    
