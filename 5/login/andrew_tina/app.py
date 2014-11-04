29176b8c75d73027e8ef551666b9e10f644cb22e
l-tn3314 <l.tn3314@gmail.com>
2014-11-04 15:00:12 -0500

    redirect working, starting mongo

--- 5/login/andrew_tina/app.py
+++ 5/login/andrew_tina/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, flash, redirect, request, render_template, url_for
+from flask import Flask, flash, redirect, request, render_template, url_for, session
+from pymongo import Connection
 
 app = Flask(__name__)
 app.secret_key = 'dont_tell'
+
+conn = Connection()
+db = conn['data']
+users = db.posts
 
 @app.route("/")
 def home():
@@ -14,13 +19,24 @@ def login():
 @app.route("/register", methods = ['GET', 'POST'])
 def register():
     error = None
-    if request.method == 'POST' or request.mthod == 'GET':
-        flash('You are now registered')
-        return redirect(url_for('login'))
+    print 'ok'
+    if request.method == 'GET':
+        return render_template('register.html')
     else:
-        #flash('Invalid username or password')
-        #return redirect(url_for("register"))
-        render_template('register.html')
+        button = request.form['b']
+        user = request.form['username']
+        pw = request.form['password']
+        if button == 'cancel':
+            return redirect(url_for('register'))
+        elif button == 'submit':
+            flash('Successfully registered!')
+            post = {"username": user,
+                    "password": pw}
+            post_id = users.insert(post)
+            post_id
+            print db.collection_names()
+            print users.find_one()
+            return redirect(url_for('login'))
         
 if __name__ == "__main__":
     app.debug = True
