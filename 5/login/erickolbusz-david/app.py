8792722066331ad10f0ccf492db33764fbe48082
erickolbusz <erickolbusz@gmail.com>
2014-11-04 15:37:33 -0500

    Login errors, basic layout finished

--- 5/login/erickolbusz-david/app.py
+++ 5/login/erickolbusz-david/app.py
@@ -1,33 +1,50 @@
 from flask import Flask, render_template, request, redirect, session
-
-#http://runnable.com/Uhf58hcCo9RSAACs/using-sessions-in-flask-for-python
+import mongo
 
 app = Flask(__name__)
 id=0
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
-def sumSessionCounter():
-  try:
-    session['counter'] += 1
-  except KeyError:
-    session['counter'] = 1
-
 #login page
 @app.route("/")
 def index():
-    return render_template ("login.html")
+    user = request.args.get("username","None")
+    pw = request.args.get("password","None")
+    if (user == "None" and pw == "None"):
+      return render_template ("login.html") #have a button that redirects to /register
+    if mongo.login(user,pw):
+      return redirect(url_for('/welcome')
+    
 
 @app.route("/register")
 def register():
-    return render_template ("register.html")
+    user = request.args.get("username","None")
+    pw = request.args.get("password","None")
+    error = mongo.add_account(user,pw)
+    if (error == 0):
+                      #why is this indentation here
+                      message = "Successfully registered"
+    if (error == 1):
+                      message = "Account already exists"
+    if (error == 2):
+                      message = "Username too short, must be at least 6 characters"
+    else:
+                      #error = 3
+                      message = "Password too short, must be at least 8 characters"
+    #flash the message
+    return render_template ("register.html") #have a button that redirects to /
     
-
+@app.route("/welcome")
+def welcome():
+    return render_template ("welcome.html") #button for other page (?) and for /logout
+                      
 @app.route('/logout')
 def clearsession():
     # Clear the session
     session.clear()
     # Redirect the user to the main page
-    return redirect(url_for('index'))
+    return redirect(url_for('/'))
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
