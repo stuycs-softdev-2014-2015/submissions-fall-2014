1898452692ceac537a9a367a7a219a49015821cc
Jessica Ng <jessica.hl.ng5@gmail.com>
2014-11-04 13:08:50 -0500

    adding stuff

--- 5/login/elia_jessica/base.py
+++ 5/login/elia_jessica/base.py
@@ -3,26 +3,43 @@ from pymongo import Connection
 
 conn = Connection()
 
-db = conn['1590']
+db = conn['1247']
 
-def addUser(usernamei, passwordi):
-    res = db.usertable.find({username:usernamei})
-    if len(res)>0:
-        return False
-    nu = {username: usernamei, password:passwordi}
-    db.usertable.save(nu)
-    return True
+
+def restart():
+    db.usertable.drop()
+    tdic = {'username': 'testing', 'password':'testing'}
+    db.usertable.insert(tdic)   
+
+def printData():
+    cres = db.usertable.find()
+    #{}, {'_id':False})
+    #print cres
+    #res = [r
+    for r in cres:
+        print r
 
 def validate(usernamei, passwordi):
-    res = db.usertable.find({username: usernamei, password:passwordi})
+    cres = db.usertable.find({'username': usernamei,'password':passwordi})
+    res = [r for r in cres]  
     if len(res)>0:
         return True
     return False
 
-def updateUser(usernamei, passwordi):
-    res = db.usertable.find({username:usernamei})
+def addUser(usernamei, passwordi):
+    cres = db.usertable.find({'username':usernamei})
+    res = [r for r in cres]
+    print res
     if len(res)>0:
         return False
-    db.usertable.update({username: usernamei, password:passwordi})
+    nu = {'username': usernamei, 'password':passwordi}
+    db.usertable.save(nu)
     return True
+
+
+def updateUser(usernamei, passwordi, passwordn):
+    if validate (usernamei, passwordi):
+        db.usertable.update ({'username':usernamei, 'password':passwordi}, {"$set": {'username': usernamei, 'password':passwordn}})
+        return True
+    return False
     
