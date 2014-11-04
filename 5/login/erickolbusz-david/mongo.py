cec27831ff73555fc9e423fb4a86ab40cb1449f4
erickolbusz <erickolbusz@gmail.com>
2014-11-04 16:37:36 -0500

    more mongo helper functions

--- 5/login/erickolbusz-david/mongo.py
+++ 5/login/erickolbusz-david/mongo.py
@@ -1,8 +1,8 @@
 from pymongo import Connection
 
 def establish_connection():
-    conn = Connection('')
-    db = conn.admin
+    conn = Connection()
+    db = conn['accountinfo']
     return db
 
 def add_account(username,password): #registration
@@ -14,7 +14,7 @@ def add_account(username,password): #registration
         return 2 #username too short
     if (len(password)<8):
         return 3 #password too short
-    db.users.insert({'name':username,'pw':password,'logincount':1})
+    db.users.insert({'name':username,'pw':password,'logincount':1,'info':""})
     return 0
 
 def login(username,password):
@@ -24,6 +24,23 @@ def login(username,password):
         user_list = db.users.find({'name':username, 'pw':password})
 	user = user_list[0]
 	new_login_count = user['logincount'] + 1
-	users.update({'name':username, 'pw':password, 'logincount':new_login_count}, upsert=True)
+	users.update({'name':username, 'pw':password, 'logincount':new_login_count, 'info':user['info']}, upsert=True)
         return True
     return False
+
+def login_count(username):
+    db = establish_connection()
+    user_list = db.users.find({'name':username})
+    user = user_list[0]
+    return user['logincount']
+
+def change_info(username,addedinfo):
+    db = establish_connection()
+    user_list = db.users.find({'name':username})
+    user = user_list[0]
+    info = user['info']
+    new_info = info + addedinfo
+    users.update({'name':username, 'pw':password, 'logincount':new_login_count, 'info':new_info}, upsert=True)
+    
+    
+    
