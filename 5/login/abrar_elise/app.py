1659f9b14c116ee53a0098809b6b00b3544f0ff5
ezlotnik <elisez97@gmail.com>
2014-11-04 00:45:09 -0500

    all pages with css and login/register function w/o flask

--- 5/login/abrar_elise/app.py
+++ 5/login/abrar_elise/app.py
@@ -1,28 +1,54 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, render_template, request, redirect, url_for,session
 from pymongo import Connection
 
 app = Flask(__name__)
+app.secret_key = "shhh"
 conn = Connection()
 db = conn ['aaez']
 
+
+
 @app.route("/")
-def index():
-    button = request.args.get("sub", None)
-    username = request.args.get("username", None)
-    passw = request.args.get("password", None)
-    if button == "Register":
+def home(): 
+    return render_template("home.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method=="POST":        
+        button = request.args.get("sub", None)
+        username = request.args.get("username", None)
+        passw = request.args.get("password", None)
+        error = None     
+        if button == "Login":
+            loginfo = { 'name': username, 'pword': passw }
+            if db.users.find_one ( { 'name' : username , 'pword' : passw } ) != None:
+                #flash("correct login info")
+                return redirect(url_for('home.html'))
+            else: 
+                flash("incorrect login info")
+                return redirect(url_for('login.html'))
+        
+    return render_template("login.html")
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.method=="POST":
+        button = request.args.get("sub", None)
+        username = request.args.get("username", None)
+        passw = request.args.get("password", None)
+        error = None
         if db.users.find_one ( { 'name' : username } ) == None:
             db.users.insert ( { 'name': username, 'pword': passw } )
-            return "<h1>Thanks for joining!</h1>"
+            #return "<h1>Thanks for joining!</h1>"
+            flash("Thanks for joining")
+            return redirect(url_for('login'))
+            #return redirect(url_for('home'))
         else:
-            return "<h1>Please select an available username</h1>"
-    elif button == "Login":
-        loginfo = { 'name': username, 'pword': passw }
-        if db.users.find_one ( { 'name' : username , 'pword' : passw } ) != None:
-            return "correct login info"
-        return "incorrect login info"
-    else:
-        return render_template("index.html")
+            #return "<h1>Please select an available username</h1>"
+            flash("Please select an available username")
+            #return redirect(url_for('register'))
+            
+    return render_template("register.html")
 
 
 if __name__=="__main__":
