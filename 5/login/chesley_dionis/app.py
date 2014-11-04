34b0e2d5bab163bc280cc6621e475cff50f2a9a4
Chesley Tan <chesleytan97@gmail.com>
2014-11-04 12:54:50 -0500

    Implement login, validation, and session;Style home page

--- 5/login/chesley_dionis/app.py
+++ 5/login/chesley_dionis/app.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template, session, redirect, request, flash
-import userdb_helper, sessiondb_helper
+from flask import Flask, render_template, session, redirect, request, flash, escape
+import userdb_helper
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    if 'SESS_ID' not in session:
-        # default value for now
-        session['SESS_ID']=0
-        
-    sess_id = session['SESS_ID']
+    if request.method == 'GET':
+        # need some validation here
+        if 'username' in session and userdb_helper.user_exists(session['username']):
+            return redirect("/home")
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if (userdb_helper.validate_login(username, password)):
+            session['username'] = request.form['username']
+            return redirect("/home")
+        else:
+            flash("Login error: Incorrect username or password.")
     return render_template("index.html", page_title="Home")
+        
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -19,30 +27,35 @@ def register():
     else:
         username = request.form['username']
         password = request.form['password']
+        result = userdb_helper.validate(username, password)
+        if (not result[0]): # invalid username or password 
+            flash(result[1])
+            return render_template("register.html", page_title="Register")
         result = userdb_helper.insert(username, password)
-        if (result[0]):
+        if (result[0]): # valid registration
             flash(result[1])
             return redirect("/")
-        else:
+        else: # user already exists
             flash(result[1])
             return render_template("register.html", page_title="Register")
 
-@app.route("/logout", methods=['POST'])
+@app.route("/logout", methods=['GET'])
 def logout():
-    session.pop('n',None)
+    session.pop('username',None)
+    flash("Successfully logged out.")
     return redirect("/")
 
 @app.route("/home")
 def home():
-    return render_template("home.html",username)
+    return render_template("home.html", page_title="Home", username=escape(session['username']))
 
 @app.route("/info")
 def info():
-    return render_template("info.html",username)
+    return render_template("info.html", page_title="Info")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 if __name__=="__main__":
     app.debug=True
