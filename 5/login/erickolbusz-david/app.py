f01f0719105184e6b592835e2f21d29e4bb228c5
David Bang <davidbang2012@gmail.com>
2014-11-04 21:46:59 -0500

    Update app.py

--- 5/login/erickolbusz-david/app.py
+++ 5/login/erickolbusz-david/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 from pymongo import Connection
 
 app = Flask(__name__)
