d930cf275a620e33e19d03db6de07faffac95f58
Benedict Bolton <benedict.bolton@gmail.com>
2014-11-05 05:22:34 -0500

    Movement of code from work directory (PortasTroiae) to submissions

---
+++ 6/login/benedicti_et_rebeccae/README.md
@@ -0,0 +1,17 @@
+PortasTroiae
+============
+
+Hail Caesar!
+
+first and foremost: omfgdogs.com
+
+his dictis
+
+This is the plan:
+- 10.30.14: do some of the mongo stuff
+  - add username and password
+  - check username and password
+- 10.31.14-11.2.14: finish the web app stuff
+  - create the other pages (mostly html/jinga stuff)
+  - getting info from the fields/ making sure the mongo stuff can work
+  - moving between pages and remembering that you're logged in
--- 6/login/benedicti_et_rebeccae/app.py
+++ 6/login/benedicti_et_rebeccae/app.py
@@ -3,9 +3,6 @@ from flask import Flask,request,redirect,render_template,session
 from pymongo import Connection,MongoClient
 
 
-#Sorry Mr.Z
-global LoginButtonPressed = False
-global RegisterButtonPressed = False
 
 ################################ mongo stuff ############################
 conn=Connection()
@@ -57,25 +54,54 @@ def authenticate (username, password):
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def base():
     #print authenticate("rebecca", "benedict")
     #print authenticate("rebecca", "yuste")
     #print check("b", "doctor")
+    #Yes this is a copy of user, but at 4:38 the time is already wasted and we will clean the code later today and for later incarnations
+    print "logging" in session
+    if "logged" not in session:
+      session['logged'] = False
     if "logging" in session:
-      if session["logging"]:
-        return render_template("home.html", logging = session["logging"])
+      logging = session["logging"]
+      print logging
+      session.pop("logging", None)
+      if logging:
+        return render_template("home.html", logging = logging, logged = session['logged'])
       else:
-        return render_template("login.html", logging = session["logging"])
+        return render_template("login.html", logging = logging, logged = session['logged'])
     if "success" in session:
-      if session["success"]:
-        return render_template("login.html", success = session["success"])
+      success = session["success"]
+      session.pop("success", None)
+      if success:
+        return render_template("login.html", success = success, logged = session['logged'])
       else:
-        return render_template("login.html", success = session['success'])
-    return render_template("login.html")
+        return render_template("register.html", success = success, logged = session['logged'])
+    return render_template("login.html", logged = session['logged'])
+
+@app.route("/register", methods=["GET", 'POST'])
+def register():
+  if "logging" in session:
+    logging = session["logging"]
+    print logging
+    session.pop("logging", None)
+    if logging:
+      return render_template("home.html", logging = logging, logged = session['logged'])
+    else:
+      return render_template("login.html", logging = logging, logged = session['logged'])
+  if "success" in session:
+    success = session["success"]
+    session.pop("success", None)
+    if success:
+      return render_template("login.html", success = success, logged = session['logged'])
+    else:
+      return render_template("register.html", success = success, logged = session['logged'])
+  return render_template("register.html", logged = session['logged'])
 
 @app.route("/logging", methods=['POST'])
 def index():
+    print "logging"
     if request.method=='POST':
         #get info in login fields, verify it
         #if it works -> send to caligula ;)
@@ -86,19 +112,21 @@ def index():
       if "user" not in session:
         session['user'] = username
       session['logging'] = True
-      return redirect("/")
+      session['logged'] = True
+      return redirect("/home")
     else:
       session['logging'] = False
       return redirect("/")
 
-@app.route("/cladius")
+#A moment of silence for our fallen code :|
+'''@app.route("/cladius")
 def test():
   if 'user' in session:
     return render_template("cladius.html")
   else:
-    return render_template('fail.html')
+    return render_template('fail.html')'''
 
-@app.route("/registering")
+@app.route("/registering", methods= ['POST'])
 def regis():
     if request.method=='POST':
         #get the info from the fields
@@ -109,58 +137,64 @@ def regis():
     if check(username, password):
       add(username,password)
       session["success"] = True
+      print session['logged']
       return redirect("/")
     else:
-      session["success"] = True
+      session["success"] = False
       return redirect("/")
-      
+
 
 @app.route("/logout")
 def logout():
     #remove session
     #return base (with message)
-    session.pop('n',None)
+    session.clear()
+    session['logged'] = False
     return redirect("/")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", logged = session['logged'])
 
-@app.route("/home") 
+#Ruuun Trees Ruuunnnn, the logging is coming!!!
+@app.route("/home")
 def home():
-    if 'user' in session:
-        return render_template("home.html")
+  if "logging" in session:
+    logging = session["logging"]
+    session.pop("logging", None)
+    if logging:
+      return render_template("home.html", logging = logging, logged = session['logged'])
     else:
-        return redirect("/")
-
+      return render_template("login.html", logging = logging, logged = session['logged'])
+  return render_template("home.html", logged=session["logged"])
 @app.route("/caesar")
 def hail():
     if 'user' in session:
-        return render_template("caesar.html")
+        return render_template("caesar.html", logged = session['logged'])
     else:
         return redirect("/")
 
 @app.route("/dogs")
 def omfg():
     if 'user' in session:
-        return render_template("dogs.html")
+        return render_template("dogs.html", logged = session['logged'])
     else:
         return redirect("/")
 
 @app.route("/caligula")
 def caligula():
     if 'user' in session:
-        return render_template("caligula.html")
+        return render_template("caligula.html", logged = session['logged'])
     else:
         return redirect("/")
 @app.route("/bees")
 def bees():
     if 'user' in session:
-        return render_template("bees.html")
+        return render_template("bees.html", logged = session['logged'])
     else:
         return redirect("/")
 
 if __name__=="__main__":
-    app.secret_key="Don't put this on github"
+    app.secret_key="Its still a project so to Github this shall go"
     app.debug=True
     app.run()
