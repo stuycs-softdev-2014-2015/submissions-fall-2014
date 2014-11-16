9226a9be41493385f8855b3e62220009dd1c9a7d
Cindy <cindw@hotmail.com>
2014-11-16 15:12:34 -0500

    decorate

--- 5/login/adriel_cindy/app.py
+++ 5/login/adriel_cindy/app.py
@@ -4,6 +4,20 @@ import mongo
 app = Flask(__name__)
 app.secret_key = 'secret'
 
+from functools import wraps
+def auth(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        print session
+        if not(mongo.exists_user(kwargs['user'])):
+            flash("There is no such user.")
+            return redirect("/")
+        if 'name' not in session:
+            flash("You don't have permission to view that user's profile.")
+            return redirect("/")
+        return func(*args)
+    return inner
+
 @app.route("/")
 def home():
     user = request.args.get("user")
@@ -19,7 +33,6 @@ def home():
             if (password == mongo.get_password(user)):
                 mongo.login_user(user)
                 session['name']=user
-                print session['name']
                 return redirect("/page")
             else:
                 flash("Username or password is not valid.")
@@ -29,6 +42,7 @@ def home():
             return redirect("/register")
         else:
             return render_template("home.html")
+
 
 @app.route("/page")
 def page():
@@ -43,26 +57,18 @@ def page():
 
 @app.route("/profile")
 @app.route("/profile/<user>")
-def profile(user):
-    if not(mongo.exists_user(user)):
-        flash("There is no such user.")
-        return redirect("/")
-    if (mongo.exists_user(user)):
-        if(session['name']==user):
-            ctr = mongo.get_info(user)
-            return render_template("profile.html", user=user, ctr=ctr)
-        else:
-            flash("You don't have permission to view that user's profile.")
-            return redirect("/")
-
+@auth
+def profile():
+    ctr = mongo.get_info(session['name'])
+    return render_template("profile.html", user=session['name'], ctr=ctr)
+ 
 @app.route("/about")
 def about(user=None):
-
     try:
         return render_template("about.html", user=session['name'])
     except:
-        print user
         return render_template("about.html", user=None)
+
 @app.route("/logout")
 def logout():
     mongo.logout_user(session['name'])
@@ -83,7 +89,6 @@ def register():
             return redirect("/register")
         if (password == pcheck and not(mongo.exists_user(user))):
             session['name']=user
-            print session['name']
             mongo.add_user(user,password)
             mongo.login_user(user)
             return redirect("/page")
