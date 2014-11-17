7ff1c65a17fc2f2ce9c77601045bd0d21169a9fd
Kingfisher <Dargodl@gmail.com>
2014-11-16 21:35:22 -0500

    Added decorator
    Checks if the user is logged in for private posts and submit. Also added
    new background for signup, tweaked the submit form

--- 7/mongoproj/Dennis-David/app.py
+++ 7/mongoproj/Dennis-David/app.py
@@ -1,8 +1,22 @@
+from functools import wraps
 from flask import Flask, session, flash, redirect, render_template, request, url_for
 import database
 import os
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+
+# Dennis and I worked on this together, and did approximately half each.
+
+def checklogin(f):
+    @wraps(f)
+    def inner(*args):
+        if 'username' not in session:
+            flash("You have to be logged in in order to view this page.")
+            return redirect(url_for('login'))
+        else:
+            return f(*args)
+    return inner
+
 @app.route('/', methods=["GET","POST"])
 @app.route('/login', methods=["GET","POST"])
 def login():
@@ -15,7 +29,7 @@ def login():
             return redirect(url_for('login'))
         flash("You've logged in successfully")
         session['username'] = request.form['username']
-        return redirect(url_for('private'))
+        return redirect(url_for("private"))
     return render_template("login.html")
 
 @app.route('/signup', methods=["GET","POST"])
@@ -30,30 +44,29 @@ def signup():
         flash("Great! You've registered! Now you can log in.")
         return redirect(url_for('login'))
     return render_template("signup.html")
+
 @app.route('/posts/public',methods=["GET","POST"])
 def public():
     posts = database.getPublicPosts()
     return render_template("public.html", posts = posts)
+
 @app.route('/posts/private',methods=["GET","POST"])
+@checklogin
 def private():
-    if 'username' in session:
-        username = session['username']
-        posts = database.getPrivatePosts() + database.getPublicPosts()
-        return render_template("private.html", posts = posts, username = username)
-    flash("You are not logged in.")
-    return redirect(url_for('login'))
+    username = session['username']
+    posts = database.getPrivatePosts() + database.getPublicPosts()
+    return render_template("private.html", posts = posts, username = username)
+
 @app.route('/posts/submit',methods=["GET","POST"])
+@checklogin
 def submit():
-    if 'username' in session:
-        if request.method == "POST":
-            if request.form["type"] == "private":
-                database.addPrivatePost(session['username'],request.form["post"])
-            else:
-                database.addPublicPost(session['username'],request.form["post"])
-            return redirect(url_for(request.form["type"])) 
-        return render_template("submit.html") 
-    flash("You are not logged in")
-    return render_template("login.html")
+    if request.method == "POST":
+        if request.form["type"] == "private":
+            database.addPrivatePost(session['username'],request.form["post"])
+        else:
+            database.addPublicPost(session['username'],request.form["post"])
+        return redirect(url_for(request.form["type"])) 
+    return render_template("submit.html") 
 
 @app.route('/logout')
 def logout():
