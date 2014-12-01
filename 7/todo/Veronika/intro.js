da0565ab298ac73e558c0feeb6fb2d2fbb43b974
Veronika <vazzara1@gmail.com>
2014-12-01 17:02:46 -0500

    prettify

--- 7/todo/Veronika/intro.js
+++ 7/todo/Veronika/intro.js
@@ -6,5 +6,17 @@ var todoCallback = function(e) {
 var todolist = document.getElementById("todolist");
 var todoitems = todolist.children;
 for(var i = 0; i < todoitems.length; i++) {
-    litems[i].addEventListener('click',todoCallback);
+    todoitems[i].addEventListener('click',todoCallback);
+}
+
+
+var doneCallback = function(e) {
+    console.log(this);
+    //this.classList.add('red');
+}
+
+var donelist = document.getElementById("donelist");
+var doneitems = donelist.children;
+for(var i = 0; i < doneitems.length; i++) {
+    doneitems[i].addEventListener('click',doneCallback);
 }
