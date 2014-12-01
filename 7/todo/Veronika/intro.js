979526bf70d034d7ad29baf9e29dde8c67e6ab4d
Veronika <vazzara1@gmail.com>
2014-12-01 16:00:56 -0500

    added bootstrap

--- 7/todo/Veronika/intro.js
+++ 7/todo/Veronika/intro.js
@@ -1,10 +1,10 @@
 var todoCallback = function(e) {
-		console.log(this);
-		#this.classList.add('red');
+    console.log(this);
+    //this.classList.add('red');
 }
 
 var todolist = document.getElementById("todolist");
 var todoitems = todolist.children;
-for(var i = 0; i < litems.length; i++) {
-		litems[i].addEventListener('click',todoCallback);
+for(var i = 0; i < todoitems.length; i++) {
+    litems[i].addEventListener('click',todoCallback);
 }
