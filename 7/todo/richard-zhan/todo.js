124b5eb33c75a1360778e987720d0a43e7b5b8af
Richard <zhan97richard@gmail.com>
2014-12-01 13:21:50 -0500

    A start

---
+++ 7/todo/richard-zhan/todo.js
@@ -0,0 +1,10 @@
+
+var td = document.getElementByID("td");
+var items = td.children;
+var b = document.getElementByID("Button");
+b.addEventListener('click',buttonCallback);
+var t = document.getElementByID("Text");
+
+
+var adding = function(x){
+    for (var i = 0; i 
