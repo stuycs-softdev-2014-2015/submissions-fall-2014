55fb8ef7a51a59d86f4706f9e38f3032bed987d8
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2014-12-01 17:05:42 -0500

    requirements met; basic assignment completed

--- 6/todo/brian_sapozhnikov/todo.js
+++ 6/todo/brian_sapozhnikov/todo.js
@@ -1,5 +1,21 @@
-var makeDone = function(){
+var todolist = document.getElementById("todolist");
+var donelist = document.getElementById("donelist");
+var button = document.getElementById("add");
 
+var markdone = function(){
+    this.removeEventListener("click", markdone);
+    donelist.appendChild(this);
+}
+
+var addTask = function(){
+    var newitem = document.createElement('li');
+    newitem.innerHTML=document.getElementById("newentry").value;
+    newitem.addEventListener('click',markdone);
+    todolist.appendChild(newitem);
+    console.log(newitem);
+    
 };
 
-var todolist = document.getElementById("todolist");
+button.addEventListener('click',addTask);
+
+
