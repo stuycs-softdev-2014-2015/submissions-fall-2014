53e39cdbde9ec4bc7aefd8c4b70ae276bc8bfb8a
Tyler Wang <twang.123@live.com>
2014-12-01 21:18:17 -0500

    check moveToDone

--- 7/todo/tianshi-lise/back.js
+++ 7/todo/tianshi-lise/back.js
@@ -1,26 +1,48 @@
-var ToDoList = document.getElementById("todo");
-var titems = ToDoList.children;
+//to do list id: todo
+//done list id: done
+//text field id: comment
+//button id: b
 
-var funct = function(e){
-    for (var i = 0; i<titems.length; i++){
-	titems[i].addEventListener('click',   );
-    }
-};
+var buttonCallback = function(e){
+    console.log(e);
+    console.log(this);
+}
 
+var button = document.getElementById("b");
+button.addEventListener('click',buttonCallback);
 
+var toDoList = document.getElementById("todo");
+var toDoItems = toDoList.children;
 
+var doneList = document.getElementById("done");
+var doneItems = doneList.children;
 
-var addItem = function(){
+var addItem = function(text){
     var todo = document.getElementById("todo");
-    var newtodo = document.getElementById("t");
-
-    var newitem = document.createElement("li");
-    newitem.innerHTML = newtodo;
-    list.appendChild(newitem);  
+    var input = document.getElementById('comment').value
+    var newtodo = document.createElement("li");
+    newtodo.innerHTML=input;
+    todo.appendChild(newtodo);
     console.log("yups");
-};
-var bb = document.getElementbyId("b");
-bb.addEventListener('click',addItem);       
+};    
+
+var moveToDone = function(e){
+    console.log(this);
+    doneList.appendChild(this);
+    console.log(doneItems.children);
+    this.parentNode.removeChild(this);
+}
+
+for(var i=0;i<toDoItems.length;i++){
+    toDoItems[i].addEventListener('click',moveToDone);
+    toDoItems[i].addEventListener('mouseover',function(e){
+	this.classList.toggle("b");
+    });
+    toDoItems[i].addEventListener('mouseout',function(e){
+	this.classList.toggle("b");
+    });
+}
+
 
 
 
