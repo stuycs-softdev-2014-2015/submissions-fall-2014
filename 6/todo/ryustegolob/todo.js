66622d25ea5da50e0e493fc3582e92dbe8ff6ea9
ryustegolob <ryustegolob@gmail.com>
2014-12-01 22:50:48 -0500

    workin

--- 6/todo/ryustegolob/todo.js
+++ 6/todo/ryustegolob/todo.js
@@ -1,1 +1,25 @@
-// To Do Jasvascript 
+var todolist = document.getElementById('ToDo');
+var donelist = document.getElementById('Done');
+
+var removeItem = function(){
+    donelist.removeChild(this);
+};
+
+var makeDone = function(){
+    todolist.removeChild(this);
+    this.removeEventListener('click',makeDone);
+    this.addEventListener('click',removeItem);
+    donelist.appendChild(this);
+};
+
+var addItem = function(){
+    var input = document.getElementById('newItem').value;
+    var newItem = document.createElement('li')
+    newItem.innerHTML = input;
+    todolist.appendChild(newItem);
+    newItem.addEventListener('click',makeDone);
+  
+};
+
+var button = document.getElementById("button");
+button.addEventListener('click',addItem);
