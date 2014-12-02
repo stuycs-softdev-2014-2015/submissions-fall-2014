adef0dd6a75ac429876c3a0494c4949e85083fdb
IsaacGluck <isaacbgluck@gmail.com>
2014-12-02 18:23:37 -0500

    topher

--- 7/cow/isaac-coby/script.js
+++ 7/cow/isaac-coby/script.js
@@ -1,32 +1,32 @@
-var mouseX, mouseY, myevent;
+var mouseX, mouseY, myEvent;
 var curposX, curposY = 5, 200;
 
 var move = function(e){
     var topher = document.getElementById("topher");
     if (mouseX > curposX){
-	curposX += 5;
-    }
-    else{
-	curposX -=5;
-    }
-    if (mouseY > curposY){
-	curposY += 5;
-    }
-    else{
-	curposY -=5;
-    }
-    if ((curposX == 300) && (curposY = 200)){
-	alert("You won");
-    }
+       curposX += 5;
+   }
+   else{
+       curposX -=5;
+   }
+   if (mouseY > curposY){
+       curposY += 5;
+   }
+   else{
+       curposY -=5;
+   }
+   if ((curposX == 300) && (curposY == 200)){
+       alert("You won");
+   }
 }
 
 window.addEventListener('mousemove',function(e){
-		mouseX = e.pageX;
-		mouseY = e.pageY;
+  mouseX = e.pageX;
+  mouseY = e.pageY;
 });
 
-function startit() {
-    myevent = setInterval(move,100);
+function begin() {
+    myEvent = setInterval(move,100);
 }
- 
-document.getElementById("start").addEventListener('click',startit);
+
+document.getElementById("start").addEventListener('click', begin);
