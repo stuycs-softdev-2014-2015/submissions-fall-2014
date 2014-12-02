7cbf4f5133b7e3edafd96ac3cb29b3b8e45be6b4
alb-yeung <alb.yeung@gmail.com>
2014-12-01 20:07:23 -0500

    can't get the page to not refresh but works

--- 7/todo/albert/todo.js
+++ 7/todo/albert/todo.js
@@ -1,6 +1,37 @@
-var addItem = function(t,id){
-    var list = document.getElementById(id)
-    var newItem = document.create("li")
-    newItem.innerHTML = t
-    list.appendChild(newItem)
+var removeItem = function(text){
+	t = text.innerHTML;
+	var items = document.getElementsByTagName('li');
+	for (var i = 0;i<items.length;i++){
+		if (items[i].innerHTML==text){
+			items[i].remove();
+		}
+	}
+	return t;
 }
+
+var addItem = function(text,id){
+    var list = document.getElementById(id);
+    var newItem = document.createElement("li");
+    newItem.innerHTML = text;
+    if (id=="todo"){
+	    newItem.addEventListener('click',function(e){
+	    	removeItem(text);
+	    	addItem(text,"done");
+	    });
+	} else {
+		newItem.addEventListener('click',function(e){
+			removeItem(text);
+		});
+	}
+    list.appendChild(newItem);
+}
+
+var doStuff = function(form){
+	addItem(form.inputtext.value,"todo");
+}
+
+var form = document.getElementById("form")
+var button = document.getElementById("submitbutton");
+button.addEventListener('click',function(e){
+	doStuff(form);
+});
