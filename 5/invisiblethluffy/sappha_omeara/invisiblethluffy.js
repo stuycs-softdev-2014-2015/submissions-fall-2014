8bc914cc7834201cf868b10efdea73e1cc86dd05
sapphacs13 <sappha.omeara@gmail.com>
2014-12-03 11:02:31 -0500

    work?

--- 5/invisiblethluffy/sappha_omeara/invisiblethluffy.js
+++ 5/invisiblethluffy/sappha_omeara/invisiblethluffy.js
@@ -4,7 +4,7 @@ var thluffy = "http://cestlaz.github.io/assets/images/thluffy.jpg";
 
 var distFromThluffy = dist(mouseX, mouseY, tX, tY);
 
-var setXY - function() {
+var setXY = function() {
     mouseX = e.pageX;
     mouseY = e.pageY;
 }
@@ -18,3 +18,8 @@ var dist = function(x1, y1, x2, y2){
 }
 
 window.addEventListener("mousemove", setXY);
+
+var getRandomXY() {
+    tX = Math.random() * screen.width;
+    tY = Math.random() * screen.height;
+}
