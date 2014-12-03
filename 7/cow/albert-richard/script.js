77313a71ee10252e0174d01ced17db7e2a008e17
alb-yeung <alb.yeung@gmail.com>
2014-12-03 18:41:58 -0500

    cleaned up code

--- 7/cow/albert-richard/script.js
+++ 7/cow/albert-richard/script.js
@@ -5,16 +5,49 @@ var size = {
     width: window.innerWidth || document.body.clientWidth,
     height: window.innerHeight || document.body.clientHeight
 };
+
 var colors = ['orange','green','purple','red','blue','yellow']; 
 var color = 0;
 var isColor = true;
+/*
+var seizure = document.getElementById("rave");
+seizure.addEventListener("click",function(e){
+    isColor=!isColor;
+});
+var colorDU = setInterval(function(e){
+    if (isColor){
+    document.body.style.background = colors[color%colors.length];
+    color++;
+    }
+},1000);
+*/
+
+//var isEasy = false;
+/*
+var easymode = document.getElementById("easy");
+easymode.addEventListener("click",funtion(e){
+    isEasy=!isEasy;
+})
+*/
 
 
-var printLocation = function(){
-    console.log(""+mouseX+","+mouseY);
-};
+var createHiddenLocation = function(e){
+    hiddenX = Math.floor(Math.random()*size.width);
+    hiddenY = Math.floor(Math.random()*size.height);
+    var pic = document.getElementById("king");
+    pic.hspace=hiddenX;
+    pic.vspace=hiddenY;
+}
+createHiddenLocation();
 
 
+var getDistance = function(e){
+    var xplace = hiddenX - mouseX;
+    var yplace = hiddenY - mouseY;
+    distance = Math.sqrt(Math.pow(xplace+10,2)+Math.pow(yplace+170,2));
+    dist = document.getElementById("dist");
+    dist.innerHTML = distance;
+}
 window.addEventListener('mousemove',function(e){
     mouseX = e.pageX;
     mouseY = e.pageY;
@@ -22,56 +55,13 @@ window.addEventListener('mousemove',function(e){
     coor.innerHTML = ""+mouseX+","+mouseY;
     getDistance();
 });
+var recalculate = setInterval(getDistance,100);
 
-/*
-var seizure = document.getElementById("rave");
-seizure.addEventListener("click",function(e){
-    isColor=!isColor;
-});
-*/
-
-var stopTimer = function(e){
-    window.clearInterval(e);
-}
 
 var popup = function(e){
     var pic = document.getElementById("king");
     pic.src="sandking.jpg"
 }
-
-var createHiddenLocation = function(e){
-    hiddenX = Math.floor(Math.random()*size.width);
-    hiddenY = Math.floor(Math.random()*size.height);
-    var pic = document.getElementById("king");
-    //pic.style.position="absolute";
-    pic.hspace=hiddenX;
-    pic.vspace=hiddenY;
-    //pic.style.display="none";
-}
-
-var getDistance = function(e){
-    var xplace = hiddenX - mouseX;
-    var yplace = hiddenY - mouseY;
-    console.log(xplace);
-    console.log(Math.pow(xplace,2));
-    distance = Math.sqrt(Math.pow(xplace+10,2)+Math.pow(yplace+170,2));
-    console.log(distance);
-    dist = document.getElementById("dist");
-    dist.innerHTML = distance;
-}
-
-
-
-createHiddenLocation();
-var recalculate = setInterval(getDistance,100);
-/*
-var colorDU = setInterval(function(e){
-    if (isColor){
-	document.body.style.background = colors[color%colors.length];
-	color++;
-    }
-},1000);
-*/
 var checkLocation = function(e){
     if (distance <= 50){
 	popup();
