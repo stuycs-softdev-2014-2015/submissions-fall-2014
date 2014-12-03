cc3f5d205c0b74523b72060fbef971dfe633c665
Angela Lin <angela97lin@gmail.com>
2014-12-02 22:31:39 -0500

    added rand stuff

--- 7/cow/angela_christina/rudolph.js
+++ 7/cow/angela_christina/rudolph.js
@@ -2,6 +2,7 @@
 
 var mouseX, mouseY;
 
+//sets our variables mouseX and mouseY to current mouse's x and y
 var mouseCoord = function(e){
     mouseX = e.pageX;
     mouseY = e.pageY;
@@ -9,5 +10,9 @@ var mouseCoord = function(e){
     console.log("y: " + mouseY);
 }
 
+//returns a random integer from min to max inclusive
+var rand = function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 window.addEventListener('mousemove', mouseCoord);
