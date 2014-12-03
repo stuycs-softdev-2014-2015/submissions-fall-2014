d160df935cdfe67788d169cc048d6a036796bb8e
alb-yeung <alb.yeung@gmail.com>
2014-12-03 18:23:02 -0500

    added blank + fixed coordinate issue
    changed sizes of sandking and fixed the weird coordinate issue of the
    picture. Also changed what procced the popup

--- 7/cow/albert-richard/script.js
+++ 7/cow/albert-richard/script.js
@@ -23,25 +23,32 @@ window.addEventListener('mousemove',function(e){
     getDistance();
 });
 
+/*
 var seizure = document.getElementById("rave");
 seizure.addEventListener("click",function(e){
     isColor=!isColor;
 });
+*/
+
 var stopTimer = function(e){
     window.clearInterval(e);
+}
+
+var popup = function(e){
+    var pic = document.getElementById("king");
+    pic.src="sandking.jpg"
 }
 
 var createHiddenLocation = function(e){
     hiddenX = Math.floor(Math.random()*size.width);
     hiddenY = Math.floor(Math.random()*size.height);
     var pic = document.getElementById("king");
-    pic.style.position="absolute";
-    pic.style.left=hiddenX;
-    pic.style.top=hiddenY;
-    pic.height="100";
-    pic.width="100";
-    pic.src="sandking.jpg";
-    pic.style.display="none";
+    //pic.style.position="absolute";
+    pic.hspace=hiddenX;
+    pic.vspace=hiddenY;
+    pic.src="blank.png";
+    pic.addEventListener('click',popup)
+    //pic.style.display="none";
 }
 
 var getDistance = function(e){
@@ -49,29 +56,27 @@ var getDistance = function(e){
     var yplace = hiddenY - mouseY;
     console.log(xplace);
     console.log(Math.pow(xplace,2));
-    distance = Math.sqrt(Math.pow(xplace,2)+Math.pow(yplace,2));
+    distance = Math.sqrt(Math.pow(xplace+10,2)+Math.pow(yplace+170,2));
     console.log(distance);
     dist = document.getElementById("dist");
     dist.innerHTML = distance;
 }
 
-var popup = function(e){
-    var pic = document.getElementById("king");
-    pic.style.display="inline-block";
-}
+
 
 createHiddenLocation();
 var recalculate = setInterval(getDistance,100);
+/*
 var colorDU = setInterval(function(e){
     if (isColor){
 	document.body.style.background = colors[color%colors.length];
 	color++;
     }
 },1000);
-
+*/
 var checkLocation = function(e){
     if (distance <= 50){
 	popup();
     }
 }
-window.addEventListener('click',checkLocation);
+//window.addEventListener('click',checkLocation);
