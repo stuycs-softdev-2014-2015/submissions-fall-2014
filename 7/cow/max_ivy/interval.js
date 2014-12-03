f9c95af8518ba738d1382026af46079c13591026
Max Zhou <ball908765@gmail.com>
2014-12-03 17:30:53 -0500

    added distance coding; this shoudl actually go into init

--- 7/cow/max_ivy/interval.js
+++ 7/cow/max_ivy/interval.js
@@ -1,6 +1,8 @@
 var IMG_COUNT = 3;
 var cowx = 0;
 var cowy = 0;
+var baseInterval = 200;
+var interval = baseInterval;
 
 var init = function(){
     var html = document.documentElement;
@@ -12,33 +14,65 @@ var init = function(){
     console.log("x: " + cowx + " y: " + cowy);
 }
 
+var d;
 var checkDist = function(e){
     //console.log(e.pageX + " " + e.pageY);
     var counter = document.getElementById("distance");
-    var d = Math.sqrt(Math.pow(cowx - e.pageX, 2)+Math.pow(cowy - e.pageY, 2));
-    console.log(d);
+    d = Math.sqrt(Math.pow(cowx - e.pageX, 2)+Math.pow(cowy - e.pageY, 2));
+    //console.log(d);
     counter.innerHTML = d;
 }
 
 
+
 var displayRandomImage = function(){
+    //adjustInterval;
+    begin();
     var src = "img" + Math.floor(Math.random()*IMG_COUNT) + ".jpg";    
     var top = Math.floor(Math.random()*document.documentElement.clientHeight) - 100 + "px";
     var right = Math.floor(Math.random()*document.documentElement.clientWidth) - 100 + "px";
-    displayImage(src,top,right);
+    displayImage(src,top,right, NaN, NaN);
+//    console.log(interval);
 }
 
-var displayImage = function(src, top, right){
+var displayImage = function(src, top, right, cursor, z){
     var img = document.createElement("img");
     img.src = src;
     img.style.position = "fixed";
     img.style.top = top;
     img.style.right = right;
     document.body.appendChild(img);
+    if(z != NaN){
+	img.style.zIndex = z
+    }
+    if (cursor != NaN){ //not good check
+	img.style.cursor = cursor;
+    }
+}
+var spawnWindows;
+
+var adjustInterval = function(){
+
+    interval = baseInterval * (d / 400);
+    if(interval < 50){
+	interval = 50;
+    }
 }
 
-var spawnWindows;
-spawnWindows = setInterval(displayRandomImage,100);
+var begin = function(){
+    clearInterval(spawnWindows);
+    adjustInterval();
+    console.log(interval);
+    spawnWindows = setInterval(displayRandomImage,interval);
+}
+
+var button = document.getElementById("begin")
+button.addEventListener("click", begin);
+
+
+//var spawnWindows;
+//spawnWindows = setInterval(displayRandomImage,100);
 
 window.addEventListener("mousemove", checkDist);
 init();
+
