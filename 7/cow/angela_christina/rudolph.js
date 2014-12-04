80f730613624f6ea704fa70eb8c501c6fae07b26
Angela Lin <angela97lin@gmail.com>
2014-12-03 22:23:15 -0500

    weeee

--- 7/cow/angela_christina/rudolph.js
+++ 7/cow/angela_christina/rudolph.js
@@ -1,15 +1,36 @@
 //CTRL-SHIFT-J TO OPEN CONSOLE (in chrome, on linux)
-
+   //displays the image if mouseX and mouseY are within a certain
+    //range and clicked --> add element to change mouse, 
+    // add source element for image
+    //maybe add a fancy zoom in stuff (inc size of image)
+    //increase score counter? if we feel like it?
+    //use a reset function (document.body.innerHTML = "";
+    //then display a replay button
+    //if replay then???????????????
+var one = new Audio('sleigh_one.mp3');
+var two = new Audio('sleigh_two.mp3');
+var three = new Audio('sleigh_three.mp3');
+var four = new Audio('sleigh_four.mp3');
+var five = new Audio('sleigh_five.mp3');
 var mouseX, mouseY;
 var randX, randY;
+var imageH = 200;
+var imageW = 150;
+var centerX, centerY;
 var playing = false; 
 var nowPlaying;
 //sets our variables mouseX and mouseY to current mouse's x and y
+
+var loop = function(e) {
+    this.currentTime = 0;
+    this.play();
+};
+
 var mouseCoord = function(e){
     mouseX = e.pageX;
     mouseY = e.pageY;
-    console.log("x: " + mouseX);
-    console.log("y: " + mouseY);
+    //console.log("x: " + mouseX);
+    //console.log("y: " + mouseY);
 }
 
 //returns a random integer from min to max inclusive
@@ -17,49 +38,74 @@ var rand = function(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+//adds an image tag to our html document
+var displayImage = function(e) {
+    var img = document.createElement("img");
+    img.src = "rudolph.png";
+    img.alt = "rudolph";
+    img.id = "rudolph";
+    img.width = imageW;
+    img.height = imageH;
+    img.style.top = randY+"px";
+    img.style.left = randX+"px";
+    //console.log(img);
+    document.body.appendChild(img);
+}
+//once per load, generates random X/Y coordinates for our image
+var placeImage = function(e){
+    randX = rand(0,window.innerWidth-imageW);
+    randY = rand(0, window.innerHeight-imageH);
+    //console.log("randX: " + randX);
+    //console.log("randY: " + randY);
+}
 
-var placeImage = function(e){//e necessary?
-    randX = rand(0,window.innerWidth);
-    randY = rand(0, window.innerHeight);
-    console.log("randX: " + randX);
-    console.log("randX: " + randY);
+//returns the general-ish coordinates of middle of our pcitures
+var centerPos = function(e){
+    centerY = randY + (imageH/2);
+    centerX = randX + (imageW/2);
+    console.log("centerY: "+centerY);
+    console.log("centerX: "+centerX);
 }
-//doesnt work lol woops
-var playMusic = function(e){
-    if(!playing){
-	this.play();
-	nowPlaying = this;
-	this.addEventListener("playing",whatMusic);
-    }
-    else{
+	    
+var playMusic = function(audio){
+    if (playing){	    
+	//console.log("nowPlaying: " + nowPlaying);
 	nowPlaying.pause();
-	this.play();
-	nowPlaying = this;
-	this.addEventListener("playing",whatMusic);
-    
     }
+    else {
+	playing = true;
+    }
+    nowPlaying = audio;
+    audio.play();
 }
+      
+var whatMusic = function(e){
+    changeX=centerX-mouseX;
+    changeY=centerY-mouseY;
+    dist= Math.sqrt(Math.pow(changeX,2) + Math.pow(changeY,2));
+    console.log("dist: "+dist);
+
+    if(dist<50){
 	
 
-    
-var whatMusic = function(e){
-    changeX=randX-mouseX;
-    changeY=randY-mouseY;
-    dist= Math.sqrt(Math.pow(changeX,2) + Math.pow(changeY,2));
-    if(dist<50){
-	var five= document.getElementById("five");
-	playing = true;
-	five.addEventListener("canplay",playMusic);
-    }
-    else if(dist < 250){
-	var four= document.getElementById("four");
-	playing = true;
-	four.addEventListener("canplay",playMusic);
-    }
+
+	else if(dist < 250){
+	if (playing){
+	   // console.log("nowPlaying: " + nowPlaying);
+	    nowPlaying.pause();
+	}
+	else {
+	    playing = true;
+	}
+	
+	nowPlaying = four;
+	four.play();
+}
+/*
     else if(dist < 450){
 	var three= document.getElementById("three");
 	playing = true;
-	three.addElementListener("canplay",playMusic);
+	three.addEventListener("canplay",playMusic);
     }
     else if(dist < 650){
 	var two= document.getElementById("two");
@@ -71,19 +117,28 @@ var whatMusic = function(e){
 	playing = true;
 	one.addEventListener("canplay",playMusic);
     }
-
+*/
 }	
-var displayImage = function(){
-    //displays the image if mouseX and mouseY are within a certain
-    //range and clicked --> add element to change mouse, 
-    // add source element for image
-    //maybe add a fancy zoom in stuff (inc size of image)
-    //increase score counter? if we feel like it?
-    //use a reset function (document.body.innerHTML = "";
-    //then display a replay button
-    //if replay then???????????????
+//http://stackoverflow.com/questions/3273552/html-5-audio-looping
+if (typeof one.loop == 'boolean')
+{
+    one.loop = true;
+    two.loop = true;
+    three.loop = true;
+    four.loop = true;
+    five.loop = true;
 }
+else
+{
+    one.addEventListener('ended', loop);
+    two.addEventListener('ended', loop);
+    three.addEventListener('ended', loop);
+    four.addEventListener('ended', loop);
+    five.addEventListener('ended', loop);
 
+}
 window.addEventListener('mousemove', mouseCoord);
 window.addEventListener('load', placeImage);
-window.addEventListener('load',whatMusic);
+window.addEventListener('load', displayImage);
+window.addEventListener('load', centerPos);
+window.addEventListener('mousemove',whatMusic);
