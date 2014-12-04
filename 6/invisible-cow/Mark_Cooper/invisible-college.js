6fa938eaa6ee3c3e39c66dff2c41518d1cac3430
Mark Norwich <mna12@aol.com>
2014-12-03 19:01:04 -0500

    Work

--- 6/invisible-cow/Mark_Cooper/invisible-college.js
+++ 6/invisible-cow/Mark_Cooper/invisible-college.js
@@ -4,22 +4,41 @@ window.addEventListener("mousemove",function(e){
 		// console.log(e.pageX+" "+e.pageY);
 		mouseX = e.pageX;
 		mouseY = e.pageY;
+    
 });
 
-var randomLocation = function(){
-    hX = random(window.innerWidth-50)
-    hY = random(window.innerHeight-50)
-    
-}; 
+
 
 var moveImage = function(){
-    var x = Math.floor(Math.random()* (window.innerWidth - 50)) + 50;
-    var y = Math.floor(Math.random()* (window.innerHeight - 50)) + 50;
+    hX = Math.floor(Math.random()* (window.innerWidth - 50)) + 50;
+    hY = Math.floor(Math.random()* (window.innerHeight - 50)) + 50;
 
     var img = document.getElementById("Harvard");
-    img.style.top = y + "px";
-    img.style.left = x + "px";
+    img.style.top = hY + "px";
+    img.style.left = hX + "px";
 };
+
+var appear = document.querySelector(".img");
+document.addEventListener("click", function(e){
+   if( mouseX >= (hX-50) &&  mouseX <= (hX+50) &&  mouseY >= (hY-50) && mouseY <= (hY+50) ){
+       appear.style.display = "none"
+       console.log("Winner")
+   }
+   else{
+       console.log("No Win")
+       
+       console.log(mouseX)
+       console.log(hX)
+       console.log(mouseY)
+       console.log(hY)
+   }
+});
+
+   /* var xPosition = e.clientX
+    var yPosition = e.clientY
+    console.log(xPosition)
+    console.log(yPosition)*/
+
 
 
 var go = function(){
