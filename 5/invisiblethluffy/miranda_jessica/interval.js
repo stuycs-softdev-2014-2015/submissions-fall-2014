84a5aed0c5461a50c18e34e51c202ecc051ce799
Claire Burghard <claireburghard@gmail.com>
2014-12-03 21:08:20 -0500

    playing with mp3

--- 5/invisiblethluffy/miranda_jessica/interval.js
+++ 5/invisiblethluffy/miranda_jessica/interval.js
@@ -1,5 +1,6 @@
 var mouseX, mouseY;
 var thluffyX, thluffyY;
+var phone = document.getElementById("phone");
 var thluffy = document.getElementById("thluffy");
 var playing= true;
 
@@ -11,56 +12,72 @@ document.getElementById("thluffy").style.left=thluffyX+"px";
 document.getElementById("thluffy").style.top=thluffyY+"px";
 
 window.addEventListener('mousemove', function(e) {
-		mouseX = e.pageX;
-		mouseY = e.pageY;
+    mouseX = e.pageX;
+    mouseY = e.pageY;
 });
 
 
 var buildup =document.getElementById('javert_loud');
 var found =document.getElementById('donotforget');
 var play = function(e) {
-	var mode=document.getElementById("mode");
-	if (mode.options[mode.selectedIndex].text == "Scare"){
-		buildup =document.getElementById('donotforget');
-		found =document.getElementById('javert_loud');
-		document.body.style.color= "#ffffff";
-		document.body.style.backgroundColor= "#000000";
-	}
-	else{
-		buildup =document.getElementById('javert_loud');
-		found =document.getElementById('donotforget');
-		document.body.style.backgroundColor= "#ffffff";
-		document.body.style.color= "#000000";
-	}
-		var distance =Math.sqrt(Math.pow(mouseX-(thluffyX+250),2)+Math.pow(mouseY-(thluffyY+110),2));
-		if (distance >500)
-			buildup.volume=.1;
-		else if (distance >400)
-			buildup.volume=.3;
-		else if (distance >300)
-			buildup.volume=.5;
-		else if (distance >200)
-			buildup.volume=.7;
-		else
-			buildup.volume=1;
-		buildup.play();
-		//console.log(distance);
-		if (playing){
-			myevent = setTimeout(play,300);
-		}
-	}
+    
+    var mode=document.getElementById("mode");
+    var distance =Math.sqrt(Math.pow(mouseX-(thluffyX+250),2)+Math.pow(mouseY-(thluffyY+110),2));
+
+    if (mode.options[mode.selectedIndex].text == "Scare"){
+
+	buildup =document.getElementById('footsteps');
+	found =document.getElementById('door');
+	document.body.style.color= "#ffffff";
+	document.body.style.backgroundColor= "#000000";
+	if (distance >500)
+	    buildup.volume=.3;
+	else if (distance >400)
+	    buildup.volume=.5;
+	else if (distance >300)
+	    buildup.volume=.7;
+	else if (distance >200)
+	    buildup.volume=1;
+	else
+	    buildup.volume=0;
+	buildup.play();
+    }
+    else{
+	buildup =document.getElementById('javert_loud');
+	found =document.getElementById('donotforget');
+	document.body.style.backgroundColor= "#ffffff";
+	document.body.style.color= "#000000";
+	if (distance >500)
+	    buildup.volume=.1;
+	else if (distance >400)
+	    buildup.volume=.3;
+	else if (distance >300)
+	    buildup.volume=.5;
+	else if (distance >200)
+	    buildup.volume=.7;
+	else
+	    buildup.volume=1;
+	buildup.play();
+    }
+    
+    //console.log(distance);
+    if (playing){
+	myevent = setTimeout(play,300);
+    }
+}
 
 var done=function(){
-	playing=false;
+    
+    playing=false;
 
-	found.volume=1;
-	buildup.pause();
-	found.play();
+    found.volume=1;
+    buildup.pause();
+    found.play();
 
-	thluffy.style.opacity=1;
+    thluffy.style.opacity=1;
 
 
-	
+    
 }
 
 thluffy.addEventListener('click',done);
