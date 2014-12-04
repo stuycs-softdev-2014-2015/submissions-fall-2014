25cecbebb028dd58a7bc4581a9d25cc239cf3df0
corrrin9797 <corrinoff@gmail.com>
2014-12-03 19:21:40 -0800

    MUAHAHAHAHAHAHA

--- 6/invisible-cow/corrin_offenholley/happy.js
+++ 6/invisible-cow/corrin_offenholley/happy.js
@@ -3,9 +3,10 @@
 
 var mouseX;
 var mouseY;
-
+var movedirx, movediry;
 var locx, locy;
-locx=Math.random()*1400 + 50
+var win = false;
+locx=Math.random()*1000 + 50
 locy=Math.random()*400 + 50
 
 console.log(locx + ' ' + locy);
@@ -18,35 +19,71 @@ window.addEventListener('mousemove',function(e){
 
 //console.log(math.random(-10,10);
 var thluffy=document.getElementById("thluffy");
+var xdir = document.createElement('li');
+var ydir = document.createElement('li');
+var bunn = document.createElement('li');
+var wins=0;
+bunn.innerHTML=wins;
 
+var mylist=document.getElementById('list')
 function move(e) {
   
   var moveelt=document.querySelector('.move');
-  var x = (moveelt.style.left);
-  var y = (moveelt.style.top);
-  x=x.substring(0,x.length-2);
-  x=parseInt(x);
-  y=y.substring(0,y.length-2);
-  y=parseInt(y);
+  var x;// = (moveelt.style.left);
+  var y;// = (moveelt.style.top);
+  //x=x.substring(0,x.length-2);
+  //x=parseInt(x);
+  //y=y.substring(0,y.length-2);
+  //y=parseInt(y);
    
   if (isNaN(x)) x=locx;
   if (isNaN(y)) y=locy;
 
   if (mouseX<x) {
-     x=x-3;
+     movedirx='right'
      
   } else {
-     x=x+3;
+     movedirx='left'
      
   }
 
   if (mouseY<y) {
-     y=y-3;
+     movediry='down'
   } else {
-     y=y+3;
+     movediry='up'
   }
-  moveelt.style.left=x+"px";
-  moveelt.style.top=y+"px";
+  //console.log(win);
+  win=(mouseY-y)*(mouseY-y)+(mouseX-x)*(mouseX-x)<2000
+  xdir.innerHTML=movedirx;
+  ydir.innerHTML=movediry;
+  //moveelt.style.left=x+"px";
+  //moveelt.style.top=y+"px";
 }
+mylist.appendChild(xdir);
+mylist.appendChild(ydir);
+
+
+
+
+var clickscare = function(e){
+	if (win) {
+		console.log('yay');
+		wins++;
+		bunn.innerHTML=wins;
+		locx=Math.random()*1000 + 50
+		locy=Math.random()*400 + 50
+		if (wins>2 && Math.random()>.5){
+			console.log('SCARY!');
+			console.log(Math.random());
+			document.body.style.background = "#f3f3f3 url('bunny.jpeg') no-repeat left top";
+			document.getElementById('audiotag1').play();
+		}
+	}
+};
+mylist.appendChild(bunn);
+document.addEventListener('click',clickscare);
+
+
+
 var myevent;
 myevent = setInterval(move,100);
