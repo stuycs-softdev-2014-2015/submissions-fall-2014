5a0a4dcd1c7e7c65d0487287168ebd6f5a9e62a2
ericwongg <wonggeric@gmail.com>
2014-12-03 21:12:56 -0500

    sounds

--- 5/invisiblethluffy/eric_jerry/timer.js
+++ 5/invisiblethluffy/eric_jerry/timer.js
@@ -16,15 +16,16 @@ var sound = function(e){
     var y = wherehow.offsetTop;
     var mouseX = Math.abs(eX-x);
     var mouseY = Math.abs(eY-y);
-    dist = Math.sqrt(mouseX*mouseX + mouseY*mouseY)/100
-    var readjustedDist = dist/10.0
+    dist = Math.sqrt(mouseX*mouseX + mouseY*mouseY)/1000
+    var readjustedDist = 1.0 - dist%1000.0
     if (dist < 0.1) {
-	wherehow.style.visibility="visible";
 	when.volume = 1.0;
 	how.volume = 1.0;
-	window.alert("Where how? WHEN HOW!");
+	when.play();
+	how.play();
     }else {
 	where.volume = readjustedDist;    
+	where.play();
     }
 }
 
@@ -36,8 +37,6 @@ var find = function(e){
     if ( (mouseX < 10) && (mouseY < 10) ) {
 	//fire
 	document.body.style.background = "#FFFFFF";
-	wherehow.style.visibility = "visible";
-	window.alert("Where how? WHEN HOW!");
     }
     else if ( (mouseX < 30) && (mouseY < 30) ) {
 	//hot
@@ -107,6 +106,11 @@ var find = function(e){
 
 var myevent;
 
+var reveal = function(){
+    wherehow.style.visibility = "visible";
+    window.alert("Where how? WHEN HOW!");
+}
+
 var startt = function(){
     document.body.style.background = "#000000";
 }
@@ -118,7 +122,7 @@ var startit = function(){
     wherehow.style.visibility = 'hidden';
     wherehow.style.left = 50 + Math.random()*1000 +'px';
     wherehow.style.top = 50 + Math.random()*500 + 'px';
-    //myevent = setInterval(find, 100);
+    myevent = setInterval(find, 100);
     myevent = setInterval(sound, 100);
 }
 
@@ -129,3 +133,4 @@ var stopit = function(){
 
 document.getElementById("start").addEventListener('click', startit);
 document.getElementById("stop").addEventListener('click', stopit);
+document.getElementById("wherehow").addEventListener('click', reveal);
