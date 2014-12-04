4e3bd515e7260550f21b8b046fa4174b5925c555
Ling <lingd1944@gmail.com>
2014-12-03 21:22:44 -0500

    revert

--- 7/cow/ling-tianshi/timer.js
+++ 7/cow/ling-tianshi/timer.js
@@ -11,11 +11,6 @@ window.addEventListener('mousemove', function(e){
     mouseY=e.pageY;
 });
 
-function calcDist(xcoord, ycoord) {
-    var ret = Math.sqrt(Math.pow((xcoord-mouseX),2)+Math.pow((ycoord-mouseY),2));
-    return ret;
-}
-
 function move(e) {
    var thluffy =document.getElementById("thluffy");
    var moveelt=document.querySelector('.move');
@@ -28,27 +23,16 @@ function move(e) {
    
    if (isNaN(x)) x=200;
    if (isNaN(y)) y=200;
-    
-  if calcDist(x, y) < 50 {
-      if (mouseX<x) {
-	  x=x+3;
-      } else {
-	  x=x-3;
-      }
-      if (mouseY<y) {
-	  y=y+3;
-      } else {
-	  y=y-3;
-      }
-  }
-  else {
-      if (x > 500){
-	  x = x-3; }
-      else { x = x+3; }
-      if (y > 500){
-	  y = y-3; }
-      else { y = y+3; }
-  }
+  if (mouseX<x) {
+     x=x+3;
+  } else {
+     x=x-3;
+   }
+  if (mouseY<y) {
+     y=y+3;
+  } else {
+     y=y-3;
+   }
    moveelt.style.left=x+"px";
    moveelt.style.top=y+"px";
   var deltaY = mouseY-y;
@@ -73,5 +57,5 @@ function stopit() {
 	window.clearTimeout(myevent);
 }
  //document.getElementById("start").addEventListener('click',startit);
-window.addEventListener('click', startit)
+document.addEventListener('click', startit())
 document.getElementById("stop").addEventListener('click',stopit);
