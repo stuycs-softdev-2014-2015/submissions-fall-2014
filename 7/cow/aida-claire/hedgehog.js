149ca01c8a74ad2f73c5895ca6274e29bb72a7fc
Aida Piccato <aidapiccato@gmail.com>
2014-12-04 02:43:41 -0500

    Added stuffff

--- 7/cow/aida-claire/hedgehog.js
+++ 7/cow/aida-claire/hedgehog.js
@@ -1,7 +1,7 @@
 var mouseX, mouseY, hedgehogX, hedgehogY;
 var hedgehog = document.getElementById("hedgehog");
 var jingle = document.getElementById("jingle");
-
+var maxDist = Math.sqrt(Math.pow(window.innerHeight,2) + Math.pow(window.innerWidth,2));
 //Setting coordinates of hedgehog at random
 hedgehogY = Math.random() * window.innerHeight * .9;
 hedgehogX = Math.random() * window.innerWidth * .9;
@@ -11,34 +11,46 @@ hedgehog.style.left = hedgehogX + "px";
 //Hiding hedgehog
 hedgehog.style.visibility = "hidden";
 
-///Adding event listener to hedgehog. Will become visible once clicked.
+///Adding event listener to hedgehog. Will become visible once clicked. 
+//Removes eventListeners from the window. Mutes jingle. 
 var reveal = function(e) {
-    hedgehog.style.visibility = "visible";
-    console.log("FOUND IT");
-};
+	if (mouseOnImg()) {
+		hedgehog.style.visibility = "visible";
+    jingle.volume = 0;
+    var text = document.createElement("h1");
+    text.innerHTML = "You Found Him! Reload page to play again!"
+    document.getElementById("heading").appendChild(text);
+    window.removeEventListener('mousemove',find);
+    window.removeEventListener('click', reveal);
+	}
+}
 
 //determines if the mouse is on the hedgehog 
 var mouseOnImg = function() {
-	if (mouseX > hedgehogX && mouseX < 150 + hedgehogX) {
-		if (mouseY > hedgehogY && mouseY < 100 + hedgehogY) {
+	if (mouseX > hedgehogX && mouseX < 225 + hedgehogX) {
+		if (mouseY > hedgehogY && mouseY < 150 + hedgehogY) {
 			return true;
 		};
 	};
 	return false;
 };
 
-//reveals hedgehog if found 
+//Runs every time mouse is moved. Calculates distance from hedgehog, adjusts volume of
+//jingle accordingly. 
 var find = function(e) {
     mouseX = e.pageX;
     mouseY = e.pageY;
     var dist;
     dist = Math.sqrt(Math.pow((mouseX - hedgehogX),2) + Math.pow((mouseY - hedgehogY),2));
-    if (dist < 200){
-	jingle.play();
-    };
-    if (mouseOnImg()) {
-    	reveal();
-    };
+   	var vol = maxDist / (90 * dist);
+   	if (vol > 1) {
+   		vol = 1;
+   	}
+   	jingle.volume = vol;
 };
 
-window.addEventListener('mousemove', find); 
+window.addEventListener('mousemove', find);
+//Adds event listener to window. Every time user clicks, program checks
+//to see if the mouse is on the image. If so, reveal! 
+window.addEventListener('click', reveal);
+	
