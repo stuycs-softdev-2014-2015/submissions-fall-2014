29d29773405964341f4828ca3715ed667e17ef76
AdamDeHovitz <adam.dehovitz@gmail.com>
2014-12-03 19:31:29 -0500

    Having problems

--- 6/invisible-cow/adam_anya/app.js
+++ 6/invisible-cow/adam_anya/app.js
@@ -5,12 +5,22 @@ var mouseX = 0;
 var mouseY = 0;
 
 var distance = function(e) {
+    //var body = document.getElementById("body");
+    var body = document.body.style.background;
+    console.log(body);
     mouseX = e.pageX;
     mouseY = e.pageY;
     //console.log(mouseX);
     //console.log(mouseY);
-    dist = Math.sqrt(((imgX-mouseX)^2)+((imgY-mouseY)^2));
+    dist = Math.sqrt(
+	Math.pow(mouseX - imgX, 2) +
+	    Math.pow(mouseY - imgY, 2)
+    )
     console.log(dist);
+    if (dist < 500){
+	//body.background-size = "15px 15px";
+	body = 'url("2048.png")';
+    }
 }
 
 document.addEventListener("mousemove", distance);
