9ddf72a9049ea56bc0b9b881367151a5f7af8b31
Alex Libman <kingalex11235@yahoo.com>
2014-12-03 23:45:32 -0500

    tweaked distance calls

--- 7/cow/alex-david/locate.js
+++ 7/cow/alex-david/locate.js
@@ -39,35 +39,39 @@ var dist = function(e){
 
 var music = function(){
 
-    if (d > 1000 && found == false){
-	document.getElementById("sound_element").innerHTML = "<embed src='"+ "banana1.wav" +"' hidden=true autostart=true loop=false>";
+    if (d > 800 && found == false){
+		document.getElementById("sound_element").innerHTML = "<embed src='"+ "banana1.wav" +"' hidden=true autostart=true loop=false>";
     }
-    if (d <= 1000 && d > 700 && found == false){
-	document.getElementById("sound_element").innerHTML = "<embed src='"+ "banana2.wav" +"' hidden=true autostart=true loop=false>";
+    if (d <= 800 && d > 500 && found == false){
+		document.getElementById("sound_element").innerHTML = "<embed src='"+ "banana2.wav" +"' hidden=true autostart=true loop=false>";
     }
-    if (d <= 700 && d > 400 && found == false){
-	document.getElementById("sound_element").innerHTML = "<embed src='"+ "banana.wav" +"' hidden=true autostart=true loop=false>";
+    if (d <= 500 && d > 250 && found == false){
+		document.getElementById("sound_element").innerHTML = "<embed src='"+ "banana.wav" +"' hidden=true autostart=true loop=false>";
     }
-    if (d <= 400 && d > 100 && found == false){
-	document.getElementById("sound_element").innerHTML = "<embed src='"+ "banana4.wav" +"' hidden=true autostart=true loop=false>";
+    if (d <= 250 && d > 80 && found == false){
+		document.getElementById("sound_element").innerHTML = "<embed src='"+ "banana4.wav" +"' hidden=true autostart=true loop=false>";
     }
-    if (d <= 100 && d >= 0 && found == false){
-	document.getElementById("sound_element").innerHTML = "<embed src='"+ "banana5.wav" +"' hidden=true autostart=true loop=false>";
+    if (d <= 80 && d >= 0 && found == false){
+		document.getElementById("sound_element").innerHTML = "<embed src='"+ "banana5.wav" +"' hidden=true autostart=true loop=false>";
     }
 };
-    
-		    
+
+
 var checkIfFound = function(e){
-    console.log("click");
+	//console.log("click");
     if (d <= 80){
-	pic.style.visibility = "visible";
-	found = true;
-	if (found = true){
-	    document.getElementById("sound_element").innerHTML = "<embed src='"+ "found.wav" +"' hidden=true autostart=true loop=false>";
+		pic.style.visibility = "visible";
+		found = true;
+		document.getElementById("sound_element").innerHTML = "<embed src='"+ "found.wav" +"' hidden=true autostart=true loop=false>";
+		window.removeEventListener("click",checkIfFound);
 	}
-    }
+};
+
+var myFunction = function() {
+    setInterval(function(){music();}, 1000);
 }
 
+myFunction();
 //var playSound = function(){
 //    while (found == false){
 //	checkIfFound();
