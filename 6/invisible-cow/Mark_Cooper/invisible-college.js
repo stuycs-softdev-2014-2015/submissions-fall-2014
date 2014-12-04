143266d60eb9d4662e7f73b066ef90a59d471e5f
coweaver <cooperweaver@mac.com>
2014-12-03 20:38:15 -0500

    guide working

--- 6/invisible-cow/Mark_Cooper/invisible-college.js
+++ 6/invisible-cow/Mark_Cooper/invisible-college.js
@@ -1,18 +1,60 @@
 var mouseX, mouseY;
 var hX, hY;
+var img = document.getElementById("Harvard");
+
 
 window.addEventListener("mousemove",function(e){
 	mouseX = e.pageX;
 	mouseY = e.pageY;
+
+	var study = document.getElementById("Study");
+	var distance = Math.sqrt(Math.pow((mouseX - hX),2) + Math.pow((mouseY - hY),2));
+	var sX, sY;
+	if (distance < 100){
+	    sX = 500;
+	    sY = 100;
+	}else if(distance < 200){
+	    sX = 400;
+	    sY = 80;
+	}else if(distance < 300){
+	    sX = 350;
+	    sY = 70;
+	}else if(distance < 400){
+	    sX = 300;
+	    sY = 60;
+	}else if(distance < 500){
+	    sX = 250;
+	    sY = 50;
+	}else if(distance < 600){
+	    sX = 200;
+	    sY = 40;
+	
+	}else if(distance < 1000){
+	    sX = 200;
+	    sY = 33;
+	}else if(distance < 2000){
+	    sX = 100;
+	    sY = 17;
+	}
+	study.style.height= sY + "px";
+	study.style.width = sX + "px";
 	
     });
 
+
+
+
 var moveImage = function(){
-    hX = Math.floor(Math.random()* (window.innerWidth - 50)) + 50;
-    hY = Math.floor(Math.random()* (window.innerHeight - 50)) + 50;
-    var img = document.getElementById("Harvard");
-    hX = 0;
-    hY = 0;
+    hX = Math.floor(Math.random()* (window.innerWidth - 25));
+    hY = Math.floor(Math.random()* (window.innerHeight - 25));
+    if (hX < 25){
+	hX = 25;
+    }
+    if (hY < 100){
+	hY = 100;
+    }
+		  
+    
     img.style.top = hY + "px";
     img.style.left = hX + "px";
 };
@@ -21,19 +63,23 @@ var moveImage = function(){
 
 window.addEventListener("click", function(e){
 	if( mouseX >= (hX-50) &&  mouseX <= (hX+50) &&  mouseY >= (hY-50) && mouseY <= (hY+50) ){
-	    var img = document.getElementById("Harvard");
-	    img.style.visibility = "visible"
-		console.log("Winner")
-		}
+	    
+	    img.style.visibility = "visible";
+	    console.log("Winner");
+	}
 	else{
-	    console.log("No Win")
+	    console.log("No Win");
 		
-		console.log(mouseX)
-		console.log(hX)
-		console.log(mouseY)
-		console.log(hY)
-		}
+	    console.log(mouseX);
+	    console.log(hX);
+	    console.log(mouseY);
+	    console.log(hY);
+	}
     });
 
 
     
+var newGame = function(){
+    //    img.style.visibility = "hidden";
+    moveImage()
+}
